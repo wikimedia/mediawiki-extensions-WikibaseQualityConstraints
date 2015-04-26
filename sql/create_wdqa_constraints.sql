100dbed220941538f22f7d01176eb6fef1ebd9f3
jeroendedauw <jeroendedauw@gmail.com>
2015-04-26 03:12:45 +0200

    Make DB setup work for installs using a table prefix
    Change-Id: I0a7ffd83677101c36c4de0ea7d064fc86535b9e3

--- sql/create_wdqa_constraints.sql
+++ sql/create_wdqa_constraints.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS wdqa_constraints (
+CREATE TABLE IF NOT EXISTS /*_*/wdqa_constraints (
   constraint_guid   		  VARCHAR(255)  	PRIMARY KEY,
   pid               		  INT(11)       	NOT NULL,
   constraint_type_qid    	VARCHAR(255)  	NOT NULL,
   constraint_parameters		TEXT			      DEFAULT NULL
-);
+) /*$wgDBTableOptions*/;
 
-CREATE INDEX wdqa_constraints_pid_index
-ON wdqa_constraints (pid);
+CREATE INDEX /*i*/wdqa_constraints_pid_index
+ON /*_*/wdqa_constraints (pid);
 
-CREATE INDEX wqda_constraints_constraint_type_qid_index
-ON wdqa_constraints (constraint_type_qid);
+CREATE INDEX /*i*/wqda_constraints_constraint_type_qid_index
+ON /*_*/wdqa_constraints (constraint_type_qid);
