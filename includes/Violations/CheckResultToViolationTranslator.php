9dd13a9f203d8533fc4eb4a3b77912e56f3a1683
addshore <addshorewiki@gmail.com>
2015-07-14 17:35:58 +0100

    Convert spaces to tabs in CheckResultToViolationTranslator.php
    Change-Id: I4035ffbada44f21c579312701a12632ff696cba0

--- includes/Violations/CheckResultToViolationTranslator.php
+++ includes/Violations/CheckResultToViolationTranslator.php
@@ -2,33 +2,32 @@
 
 namespace WikibaseQuality\ConstraintReport\Violations;
 
+use InvalidArgumentException;
 use Wikibase\DataModel\Entity\Entity;
 use Wikibase\DataModel\Statement\Statement;
 use Wikibase\Lib\Store\EntityRevisionLookup;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\CheckResult;
 use WikibaseQuality\Violations\Violation;
-use InvalidArgumentException;
-
 
 class CheckResultToViolationTranslator {
 
-    /**
-     * @var EntityRevisionLookup
-     */
-    private $entityRevisionLookup;
+	/**
+	 * @var EntityRevisionLookup
+	 */
+	private $entityRevisionLookup;
 
-    /**
-     * @param EntityRevisionLookup $entityRevisionLookup
-     */
-    public function __construct( EntityRevisionLookup $entityRevisionLookup ) {
-        $this->entityRevisionLookup = $entityRevisionLookup;
-    }
+	/**
+	 * @param EntityRevisionLookup $entityRevisionLookup
+	 */
+	public function __construct( EntityRevisionLookup $entityRevisionLookup ) {
+		$this->entityRevisionLookup = $entityRevisionLookup;
+	}
 
 	public function translateToViolation( Entity $entity, $checkResultOrArray ) {
 
-	    $checkResultArray = $this->setCheckResultArray( $checkResultOrArray );
+		$checkResultArray = $this->setCheckResultArray( $checkResultOrArray );
 
-		$violationArray = array ();
+		$violationArray = array();
 		foreach ( $checkResultArray as $checkResult ) {
 			if ( $checkResult->getStatus() !== CheckResult::STATUS_VIOLATION ) {
 				continue;
@@ -40,48 +39,64 @@ class CheckResultToViolationTranslator {
 			$entityId = $entity->getId();
 			//TODO: Use real claimGuid
 			$constraintTypeEntityId = $checkResult->getConstraintName();
-            $constraintId = $this->setConstraintId( $checkResult, $statement, $constraintTypeEntityId );
+			$constraintId =
+				$this->setConstraintId( $checkResult, $statement, $constraintTypeEntityId );
 			$revisionId = $this->entityRevisionLookup->getLatestRevisionId( $entityId );
 			$status = CheckResult::STATUS_VIOLATION;
 
-            $constraintParameters = $checkResult->getParameters();
+			$constraintParameters = $checkResult->getParameters();
 
-            $additionalInfo = array();
+			$additionalInfo = array();
 
-            if ( array_key_exists( 'constraint_status', $constraintParameters )) {
-                $additionalInfo['constraint_status'] = $constraintParameters['constraint_status'];
-            }
+			if ( array_key_exists( 'constraint_status', $constraintParameters ) ) {
+				$additionalInfo['constraint_status'] = $constraintParameters['constraint_status'];
+			}
 
-            $violationArray[ ] = new Violation( $entityId, $propertyId, $claimGuid, $constraintId, $constraintTypeEntityId, $revisionId, $status, $additionalInfo );
+			$violationArray[] =
+				new Violation(
+					$entityId,
+					$propertyId,
+					$claimGuid,
+					$constraintId,
+					$constraintTypeEntityId,
+					$revisionId,
+					$status,
+					$additionalInfo
+				);
 		}
 
 		return $violationArray;
 	}
 
-    /**
-     * @param CheckResult|CheckResult[] $checkResultOrArray
-     * @return CheckResult[]
-     */
-    private function setCheckResultArray( $checkResultOrArray ){
+	/**
+	 * @param CheckResult|CheckResult[] $checkResultOrArray
+	 *
+	 * @return CheckResult[]
+	 */
+	private function setCheckResultArray( $checkResultOrArray ) {
 
-        if ( $checkResultOrArray instanceof CheckResult ) {
-            return array ( $checkResultOrArray );
-        } elseif ( is_array( $checkResultOrArray ) ) {
-            return $checkResultOrArray;
-        }
+		if ( $checkResultOrArray instanceof CheckResult ) {
+			return array( $checkResultOrArray );
+		} elseif ( is_array( $checkResultOrArray ) ) {
+			return $checkResultOrArray;
+		}
 
-        throw new InvalidArgumentException;
-    }
+		throw new InvalidArgumentException;
+	}
 
-    private function setConstraintId( CheckResult $checkResult, Statement $statement, $constraintTypeEntityId  ){
-        $constraintId = $statement->getGuid() . $constraintTypeEntityId;
-        $parameters = $checkResult->getParameters();
-        if ( is_array( $parameters ) ) {
-            foreach ( $parameters as $par ) {
-                $constraintId .= implode( ', ', $par );
-            }
-        }
+	private function setConstraintId(
+		CheckResult $checkResult,
+		Statement $statement,
+		$constraintTypeEntityId
+	) {
+		$constraintId = $statement->getGuid() . $constraintTypeEntityId;
+		$parameters = $checkResult->getParameters();
+		if ( is_array( $parameters ) ) {
+			foreach ( $parameters as $par ) {
+				$constraintId .= implode( ', ', $par );
+			}
+		}
 
-        return WBQ_CONSTRAINTS_ID. Violation::CONSTRAINT_ID_DELIMITER . $constraintId;
-    }
+		return WBQ_CONSTRAINTS_ID . Violation::CONSTRAINT_ID_DELIMITER . $constraintId;
+	}
 }
