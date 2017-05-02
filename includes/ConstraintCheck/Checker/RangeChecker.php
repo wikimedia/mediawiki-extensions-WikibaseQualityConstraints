367381f19af103b4b0e6560f169d0c220a9816a6
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-05-02 18:37:57 +0200

    Reduce duplication and split long lines in RangeChecker
    Bug: T164087
    Change-Id: I21999c5ab17a16a4e713de83a252f1467248244a

--- includes/ConstraintCheck/Checker/RangeChecker.php
+++ includes/ConstraintCheck/Checker/RangeChecker.php
@@ -74,35 +74,61 @@ class RangeChecker implements ConstraintChecker {
 		 *   parameters (minimum_quantity and maximum_quantity) or (minimum_date and maximum_date) must not be null
 		 */
 		if ( $dataValue->getType() === 'quantity' ) {
-			if ( array_key_exists( 'minimum_quantity', $constraintParameters ) && array_key_exists( 'maximum_quantity', $constraintParameters ) && !array_key_exists( 'minimum_date', $constraintParameters ) && !array_key_exists( 'maximum_date', $constraintParameters ) ) {
+			if ( array_key_exists( 'minimum_quantity', $constraintParameters )
+				&& array_key_exists( 'maximum_quantity', $constraintParameters )
+				&& !array_key_exists( 'minimum_date', $constraintParameters )
+				&& !array_key_exists( 'maximum_date', $constraintParameters )
+			) {
 				$min = $constraintParameters['minimum_quantity'];
 				$max = $constraintParameters['maximum_quantity'];
-				$parameters['minimum_quantity'] = $this->constraintParameterParser->parseSingleParameter( $constraintParameters['minimum_quantity'], true );
-				$parameters['maximum_quantity'] = $this->constraintParameterParser->parseSingleParameter( $constraintParameters['maximum_quantity'], true );
+				$parameters['minimum_quantity'] = $this->constraintParameterParser->parseSingleParameter( $min, true );
+				$parameters['maximum_quantity'] = $this->constraintParameterParser->parseSingleParameter( $max, true );
 			} else {
-				$message = wfMessage( "wbqc-violation-message-range-parameters-needed" )->params( 'quantity', 'minimum_quantity', 'maximum_quantity' )->escaped();
+				$message = wfMessage( 'wbqc-violation-message-range-parameters-needed' )
+					->params( 'quantity', 'minimum_quantity', 'maximum_quantity' )->escaped();
 			}
 		} elseif ( $dataValue->getType() === 'time' ) {
-			if ( !array_key_exists( 'minimum_quantity', $constraintParameters ) && !array_key_exists( 'maximum_quantity', $constraintParameters ) && array_key_exists( 'minimum_date', $constraintParameters ) && array_key_exists( 'maximum_date', $constraintParameters ) ) {
+			if ( !array_key_exists( 'minimum_quantity', $constraintParameters )
+				&& !array_key_exists( 'maximum_quantity', $constraintParameters )
+				&& array_key_exists( 'minimum_date', $constraintParameters )
+				&& array_key_exists( 'maximum_date', $constraintParameters )
+			) {
 				$min = $constraintParameters['minimum_date'];
 				$max = $constraintParameters['maximum_date'];
-				$parameters['minimum_date'] = $this->constraintParameterParser->parseSingleParameter( $constraintParameters['minimum_date'], true );
-				$parameters['maximum_date'] = $this->constraintParameterParser->parseSingleParameter( $constraintParameters['maximum_date'], true );
-			} else if ( array_key_exists( 'minimum_quantity', $constraintParameters ) && array_key_exists( 'maximum_quantity', $constraintParameters ) && !array_key_exists( 'minimum_date', $constraintParameters ) && !array_key_exists( 'maximum_date', $constraintParameters ) ) {
-				// temporary workaround for T164087: ConstraintsFromTemplates always calls the parameters _quantity, even for time properties, so fall back to that
-				// TODO remove this `else if` once we only import constraints from statements and the other ones have been removed from the constraints table in the database
+				$parameters['minimum_date'] = $this->constraintParameterParser->parseSingleParameter( $min, true );
+				$parameters['maximum_date'] = $this->constraintParameterParser->parseSingleParameter( $max, true );
+			} elseif ( array_key_exists( 'minimum_quantity', $constraintParameters )
+				&& array_key_exists( 'maximum_quantity', $constraintParameters )
+				&& !array_key_exists( 'minimum_date', $constraintParameters )
+				&& !array_key_exists( 'maximum_date', $constraintParameters )
+			) {
+				// Temporary workaround for T164087: ConstraintsFromTemplates always calls the
+				// parameters …_quantity, even for time properties, so fall back to that.
+				// TODO: Remove this `elseif` once we only import constraints from statements and
+				// the other ones have been removed from the constraints table in the database.
 				$min = $constraintParameters['minimum_quantity'];
 				$max = $constraintParameters['maximum_quantity'];
-				$parameters['minimum_date'] = $this->constraintParameterParser->parseSingleParameter( $constraintParameters['minimum_quantity'], true );
-				$parameters['maximum_date'] = $this->constraintParameterParser->parseSingleParameter( $constraintParameters['maximum_quantity'], true );
+				$parameters['minimum_date'] = $this->constraintParameterParser->parseSingleParameter( $min, true );
+				$parameters['maximum_date'] = $this->constraintParameterParser->parseSingleParameter( $max, true );
 			} else {
-				$message = wfMessage( "wbqc-violation-message-range-parameters-needed" )->params( 'time', 'minimum_date', 'maximum_date' )->escaped();
+				$message = wfMessage( 'wbqc-violation-message-range-parameters-needed' )
+					->params( 'time', 'minimum_date', 'maximum_date' )->escaped();
 			}
 		} else {
-			$message = wfMessage( "wbqc-violation-message-value-needed-of-types-2" )->params( $constraint->getConstraintTypeName(), 'quantity', 'time' )->escaped();
+			$message = wfMessage( 'wbqc-violation-message-value-needed-of-types-2' )
+				->params( $constraint->getConstraintTypeName(), 'quantity', 'time' )->escaped();
 		}
+
 		if ( isset( $message ) ) {
-			return new CheckResult( $entity->getId(), $statement, $constraint->getConstraintTypeQid(), $constraint->getConstraintId(), $parameters, CheckResult::STATUS_VIOLATION, $message );
+			return new CheckResult(
+				$entity->getId(),
+				$statement,
+				$constraint->getConstraintTypeQid(),
+				$constraint->getConstraintId(),
+				$parameters,
+				CheckResult::STATUS_VIOLATION,
+				$message
+			);
 		}
 
 		$comparativeValue = $this->rangeCheckerHelper->getComparativeValue( $dataValue );
@@ -115,7 +141,15 @@ class RangeChecker implements ConstraintChecker {
 			$status = CheckResult::STATUS_COMPLIANCE;
 		}
 
-		return new CheckResult( $entity->getId(), $statement, $constraint->getConstraintTypeQid(), $constraint->getConstraintId(), $parameters, $status, $message );
+		return new CheckResult(
+			$entity->getId(),
+			$statement,
+			$constraint->getConstraintTypeQid(),
+			$constraint->getConstraintId(),
+			$parameters,
+			$status,
+			$message
+		);
 	}
 
 }
