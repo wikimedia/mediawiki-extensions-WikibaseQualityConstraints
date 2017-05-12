0efb8b7b611dceeb42757fdd25f1f1424b72709d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-05-12 12:40:52 +0200

    Add expectation for number of calls
    If the cycle detection in TypeCheckerHelper doesn’t work, it will now
    cause a test failure instead of a test that runs forever.
    
    Includes a refactoring to turn a member variable initialized in setUp()
    into a getter function – expectations on mocks created in setUp() have
    no effect, so it’s better to create the mocks in the context of the test
    method.
    
    Change-Id: I231421a52bff138e17d60696b66f090cd8d0ea22

--- tests/phpunit/Checker/TypeChecker/TypeCheckerHelperTest.php
+++ tests/phpunit/Checker/TypeChecker/TypeCheckerHelperTest.php
@@ -4,6 +4,7 @@ namespace WikibaseQuality\ConstraintReport\Test\TypeChecker;
 
 use PHPUnit_Framework_TestCase;
 use Wikibase\DataModel\Services\EntityId\PlainEntityIdFormatter;
+use Wikibase\DataModel\Services\Lookup\EntityLookup;
 use Wikibase\DataModel\Statement\Statement;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\DataModel\Entity\EntityIdValue;
@@ -27,59 +28,63 @@ class TypeCheckerHelperTest extends PHPUnit_Framework_TestCase {
 	use DefaultConfig;
 
 	/**
-	 * @var TypeCheckerHelper
+	 * @return TypeCheckerHelper
 	 */
-	private $helper;
-
-	protected function setUp() {
-		parent::setUp();
-		$this->helper = new TypeCheckerHelper(
+	private function getHelper() {
+		return new TypeCheckerHelper(
 			new JsonFileEntityLookup( __DIR__ ),
 			$this->getDefaultConfig(),
 			new PlainEntityIdFormatter()
 		);
 	}
 
-	protected function tearDown() {
-		unset( $this->helper );
-		parent::tearDown();
+	/**
+	 * @return TypeCheckerHelper asserts that the helper calls getEntity exactly MAX_ENTITIES times
+	 */
+	private function getMaxEntitiesHelper() {
+		$realLookup = new JsonFileEntityLookup( __DIR__ );
+		$lookup = $this->getMockBuilder( EntityLookup::class )->getMock();
+		$lookup->expects( $this->exactly( TypeCheckerHelper::MAX_ENTITIES ) )
+			->method( 'getEntity' )
+			->will( $this->returnCallback( [ $realLookup, 'getEntity' ] ) );
+		return new TypeCheckerHelper( $lookup, $this->getDefaultConfig(), new PlainEntityIdFormatter() );
 	}
 
 	public function testCheckHasClassInRelationValid() {
 		$statement1 = new Statement( new PropertyValueSnak( new PropertyId( 'P1' ), new EntityIdValue( new ItemId( 'Q42' ) ) ) );
 		$statement2 = new Statement( new PropertyValueSnak( new PropertyId( 'P31' ), new EntityIdValue( new ItemId( 'Q1' ) ) ) );
 		$statements = new StatementList( [ $statement1, $statement2 ] );
-		$this->assertTrue( $this->helper->hasClassInRelation( $statements, 'P31', [ 'Q1' ] ) );
+		$this->assertTrue( $this->getHelper()->hasClassInRelation( $statements, 'P31', [ 'Q1' ] ) );
 	}
 
 	public function testCheckHasClassInRelationInvalid() {
 		$statement1 = new Statement( new PropertyValueSnak( new PropertyId( 'P1' ), new EntityIdValue( new ItemId( 'Q42' ) ) ) );
 		$statement2 = new Statement( new PropertyValueSnak( new PropertyId( 'P31' ), new EntityIdValue( new ItemId( 'Q100' ) ) ) );
 		$statements = new StatementList( [ $statement1, $statement2 ] );
-		$this->assertFalse( $this->helper->hasClassInRelation( $statements, 'P31', [ 'Q1' ] ) );
+		$this->assertFalse( $this->getHelper()->hasClassInRelation( $statements, 'P31', [ 'Q1' ] ) );
 	}
 
 	public function testCheckHasClassInRelationValidWithIndirection() {
 		$statement1 = new Statement( new PropertyValueSnak( new PropertyId( 'P1' ), new EntityIdValue( new ItemId( 'Q42' ) ) ) );
 		$statement2 = new Statement( new PropertyValueSnak( new PropertyId( 'P31' ), new EntityIdValue( new ItemId( 'Q5' ) ) ) );
 		$statements = new StatementList( [ $statement1, $statement2 ] );
-		$this->assertTrue( $this->helper->hasClassInRelation( $statements, 'P31', [ 'Q4' ] ) );
+		$this->assertTrue( $this->getHelper()->hasClassInRelation( $statements, 'P31', [ 'Q4' ] ) );
 	}
 
 	public function testCheckIsSubclassOfValidWithIndirection() {
-		$this->assertTrue( $this->helper->isSubclassOf( new ItemId( 'Q6' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertTrue( $this->getHelper()->isSubclassOf( new ItemId( 'Q6' ), [ 'Q100', 'Q101' ] ) );
 	}
 
 	public function testCheckIsSubclassOfInvalid() {
-		$this->assertFalse( $this->helper->isSubclassOf( new ItemId( 'Q6' ), [ 'Q200', 'Q201' ] ) );
+		$this->assertFalse( $this->getHelper()->isSubclassOf( new ItemId( 'Q6' ), [ 'Q200', 'Q201' ] ) );
 	}
 
 	public function testCheckIsSubclassCyclic() {
-		$this->assertFalse( $this->helper->isSubclassOf( new ItemId( 'Q7' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertFalse( $this->getMaxEntitiesHelper()->isSubclassOf( new ItemId( 'Q7' ), [ 'Q100', 'Q101' ] ) );
 	}
 
 	public function testCheckIsSubclassCyclicWide() {
-		$this->assertFalse( $this->helper->isSubclassOf( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertFalse( $this->getMaxEntitiesHelper()->isSubclassOf( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
 	}
 
 }
