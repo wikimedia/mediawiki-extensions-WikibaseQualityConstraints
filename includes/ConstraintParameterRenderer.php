44c1276f38726d594d892a94c0936b04322d3924
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-05-15 18:00:54 +0200

    Add useful methods to ConstraintParameterRenderer
    These methods can be used by individual checkers to render constraint
    parameters into their messages (the intention is to inject a
    ConstraintParameterRenderer into them instead of the current
    EntityIdFormatter).
    
    The implode( '</li><li>', … ) trick is taken from Ie3a3e1af93.
    
    Change-Id: I5666f3b062a37793a5f5f7e88912aa2d478a6eb7

--- includes/ConstraintParameterRenderer.php
+++ includes/ConstraintParameterRenderer.php
@@ -2,6 +2,7 @@
 namespace WikibaseQuality\ConstraintReport;
 
 use DataValues\DataValue;
+use InvalidArgumentException;
 use ValueFormatters\ValueFormatter;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\ItemId;
@@ -39,8 +40,8 @@ class ConstraintParameterRenderer {
 	private $dataValueFormatter;
 
 	/**
-	 * @param EntityIdFormatter $entityIdFormatter
-	 * @param ValueFormatter $dataValueFormatter
+	 * @param EntityIdFormatter $entityIdFormatter should return HTML
+	 * @param ValueFormatter $dataValueFormatter should return HTML
 	 */
 	public function __construct(
 		EntityIdFormatter $entityIdFormatter,
@@ -64,10 +65,10 @@ class ConstraintParameterRenderer {
 			return htmlspecialchars( $value );
 		} elseif ( $value instanceof EntityId ) {
 			// Cases like 'Conflicts with' 'property', to which we can link
-			return $this->entityIdLabelFormatter->formatEntityId( $value );
+			return $this->formatEntityId( $value );
 		} else {
 			// Cases where we format a DataValue
-			return $this->dataValueFormatter->format( $value );
+			return $this->formatDataValue( $value );
 		}
 	}
 
@@ -111,6 +112,110 @@ class ConstraintParameterRenderer {
 		}
 
 		return $array;
+	}
+
+	/**
+	 * @param DataValue $value
+	 * @return string HTML
+	 */
+	public function formatDataValue( DataValue $value ) {
+		return $this->dataValueFormatter->format( $value );
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 * @return string HTML
+	 */
+	public function formatEntityId( EntityId $entityId ) {
+		return $this->entityIdLabelFormatter->formatEntityId( $entityId );
+	}
+
+	/**
+	 * Format a property ID parameter, potentially unparsed (string).
+	 *
+	 * If you know that your property ID is already parsed, use {@see formatEntityId}.
+	 *
+	 * @param PropertyId|string $propertyId
+	 * @return string HTML
+	 */
+	public function formatPropertyId( $propertyId ) {
+		if ( $propertyId instanceof PropertyId ) {
+			return $this->formatEntityId( $propertyId );
+		} elseif ( is_string( $propertyId ) ) {
+			try {
+				return $this->formatEntityId( new PropertyId( $propertyId ) );
+			} catch ( InvalidArgumentException $e ) {
+				return htmlspecialchars( $propertyId );
+			}
+		} else {
+			throw new InvalidArgumentException( '$propertyId must be either PropertyId or string' );
+		}
+	}
+
+	/**
+	 * Format an item ID parameter, potentially unparsed (string).
+	 *
+	 * If you know that your item ID is already parsed, use {@see formatEntityId}.
+	 *
+	 * @param ItemId|string $itemId
+	 * @return string HTML
+	 */
+	public function formatItemId( $itemId ) {
+		if ( $itemId instanceof ItemId ) {
+			return $this->formatEntityId( $itemId );
+		} elseif ( is_string( $itemId ) ) {
+			try {
+				return $this->formatEntityId( new ItemId( $itemId ) );
+			} catch ( InvalidArgumentException $e ) {
+				return htmlspecialchars( $itemId );
+			}
+		} else {
+			throw new InvalidArgumentException( '$itemId must be either ItemId or string' );
+		}
+	}
+
+	/**
+	 * Format a list of (potentially unparsed) property IDs.
+	 *
+	 * The returned array begins with an HTML list of the formatted property IDs
+	 * and then contains all the individual formatted property IDs.
+	 *
+	 * @param PropertyId[]|string[] $propertyIds
+	 * @return string[] HTML
+	 */
+	public function formatPropertyIdList( array $propertyIds ) {
+		if ( empty( $propertyIds ) ) {
+			return [ '<ul></ul>' ];
+		}
+		$propertyIds = $this->limitArrayLength( $propertyIds );
+		$formattedPropertyIds = array_map( [ $this, "formatPropertyId" ], $propertyIds );
+		array_unshift(
+			$formattedPropertyIds,
+			'<ul><li>' . implode( '</li><li>', $formattedPropertyIds ) . '</li></ul>'
+		);
+		return $formattedPropertyIds;
+	}
+
+	/**
+	 * Format a list of (potentially unparsed) item IDs.
+	 *
+	 * The returned array begins with an HTML list of the formatted item IDs
+	 * and then contains all the individual formatted item IDs.
+	 *
+	 * @param ItemId[]|string[] $itemIds
+	 * @return string[] HTML
+	 */
+	public function formatItemIdList( array $itemIds ) {
+		if ( empty( $itemIds ) ) {
+			return [ '<ul></ul>' ];
+		}
+		$itemIds = $this->limitArrayLength( $itemIds );
+		$formattedItemIds = array_map( [ $this, "formatItemId" ], $itemIds );
+		array_unshift(
+			$formattedItemIds,
+			'<ul><li>' . implode( '</li><li>', $formattedItemIds ) . '</li></ul>'
+		);
+		return $formattedItemIds;
 	}
 
 }
