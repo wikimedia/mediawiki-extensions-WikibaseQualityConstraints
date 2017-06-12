3a820cf35fe1320dbc814c16de3208c18c2a96ab
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-06-12 16:22:51 +0200

    Add @throws ConstraintParameterException to checkConstraint
    Change-Id: I724575c56291e188b9bfccc4b1003034d4a0716d

--- includes/ConstraintCheck/ConstraintChecker.php
+++ includes/ConstraintCheck/ConstraintChecker.php
@@ -6,6 +6,7 @@ use Wikibase\DataModel\Entity\EntityDocument;
 use Wikibase\DataModel\Statement\Statement;
 use Wikibase\DataModel\Statement\StatementListProvider;
 use WikibaseQuality\ConstraintReport\Constraint;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintParameterException;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\SparqlHelperException;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\CheckResult;
 
@@ -18,6 +19,7 @@ interface ConstraintChecker {
 	 *
 	 * @return CheckResult
 	 *
+	 * @throws ConstraintParameterException if the constraint parameters are invalid
 	 * @throws SparqlHelperException if the checker uses SPARQL and the query times out or some other error occurs
 	 */
 	public function checkConstraint( Statement $statement, Constraint $constraint, EntityDocument $entity );
