d475192ccce77477e51c9c0c0b6e5fe00ebfd2ea
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-06-19 12:48:59 +0200

    Import Range constraints from statements
    RangeChecker gets a ConstraintStatementParameterParser injected, which
    it uses to parse the range parameters. A config variable for the
    constraint type item is added, and ConstraintReportFactory aliases it to
    the “Range” constraint type.
    
    RangeCheckerHelper learns to compare null values, in order to support
    half-open ranges. The violation message gets −∞ and ∞ as arguments in
    that case, which is a stop-gap solution until those cases get dedicated
    messages in T168268.
    
    Since ConstraintStatementParameterParser only supports …_quantity
    template parameters (the template exporter never produced …_date), all
    the tests with …_date parameters are updated to specify …_quantity
    instead. Tests for incorrect parameters are removed, since they are now
    covered by the ConstraintStatementParameterParser tests and throw
    exceptions instead of returning constraint violation check results.
    
    New tests are added to test half-open ranges; to support this without
    too much copied boilerplate code, the ConstraintParameters test trait
    gains a method to create range parameters.
    
    Bug: T167413
    Change-Id: Ide78cc0e79374cc682c99467cde3de742a51d3d0

--- tests/phpunit/ConstraintParameters.php
+++ tests/phpunit/ConstraintParameters.php
@@ -2,11 +2,13 @@
 
 namespace WikibaseQuality\ConstraintReport\Tests;
 
+use DataValues\DataValue;
 use Serializers\Serializer;
 use ValueFormatters\ValueFormatter;
 use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Services\EntityId\PlainEntityIdFormatter;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintStatementParameterParser;
 use WikibaseQuality\ConstraintReport\ConstraintParameterRenderer;
@@ -90,6 +92,34 @@ trait ConstraintParameters {
 				},
 				$properties
 			)
+		];
+	}
+
+	/**
+	 * @param string $type 'quantity' or 'time'
+	 * @param DataValue|null $min lower boundary, or null to signify no lower boundary
+	 * @param DataValue|null $max upper boundary, or null to signfiy no upper boundary
+	 * @return array
+	 */
+	public function rangeParameter( $type, DataValue $min = null, DataValue $max = null ) {
+		$configKey = $type === 'quantity' ? 'Quantity' : 'Date';
+		$config = $this->getDefaultConfig();
+		$minimumId = $config->get( 'WBQualityConstraintsMinimum' . $configKey . 'Id' );
+		$maximumId = $config->get( 'WBQualityConstraintsMaximum' . $configKey . 'Id' );
+		if ( $min === null ) {
+			$minimumSnak = new PropertyNoValueSnak( new PropertyId( $minimumId ) );
+		} else {
+			$minimumSnak = new PropertyValueSnak( new PropertyId( $minimumId ), $min );
+		}
+		if ( $max === null ) {
+			$maximumSnak = new PropertyNoValueSnak( new PropertyId( $maximumId ) );
+		} else {
+			$maximumSnak = new PropertyValueSnak( new PropertyId( $maximumId ), $max );
+		}
+		$snakSerializer = $this->getSnakSerializer();
+		return [
+			$minimumId => [ $snakSerializer->serialize( $minimumSnak ) ],
+			$maximumId => [ $snakSerializer->serialize( $maximumSnak ) ]
 		];
 	}
 
