eb9e44ba046e8c845a8fccf98a82de0104b3fff9
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-07-07 14:40:21 +0200

    Add checkconstraintparameters API action
    This new API action checks the validity of constraint parameters for a
    given set of constraints, or for all constraints defined on a given set
    of properties. The returned structure is meant to be extensible, as
    later commits may add more information to ConstraintParameterException
    objects (e. g. the affected parameter or the message key).
    
    Note that this API only supports constraint IDs that are valid statement
    GUIDs, i. e. constraints imported from statements.
    
    Bug: T169531
    Change-Id: I91f8b607ce1b37e5718f710058727caf5054166c

---
+++ tests/phpunit/Api/CheckConstraintParametersTest.php
@@ -0,0 +1,353 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\Tests\Api;
+
+use ApiTestCase;
+use RequestContext;
+use Wikibase\Repo\WikibaseRepo;
+use WikibaseQuality\ConstraintReport\Api\CheckConstraintParameters;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\DelegatingConstraintChecker;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintParameterException;
+
+/**
+ * @covers \WikibaseQuality\ConstraintReport\Api\CheckConstraintParameters
+ *
+ * @group API
+ * @group Database
+ * @group Wikibase
+ * @group WikibaseAPI
+ *
+ * @group medium
+ *
+ * @license GPL-2.0+
+ */
+class CheckConstraintParametersTest extends ApiTestCase {
+
+	const P1 = 'P1';
+	const P2 = 'P2';
+	const P1_NONEXISTENT = 'P1$febf1ef9-9291-4599-8488-00dd5f6ac814';
+	const P1_GOOD = 'P1$c76b6bf6-9c4f-4ef0-afb0-78cae372453d';
+	const P1_BAD = 'P1$4f7462a4-4523-4371-a35c-d246fa7159ee';
+	const P2_GOOD = 'P2$82648667-ca79-40e1-8839-4ed4abbb4c6d';
+
+	private $oldModuleDeclaration;
+
+	/**
+	 * @var callable
+	 */
+	private $checkConstraintParametersOnPropertyId;
+
+	/**
+	 * @var callable
+	 */
+	private $checkConstraintParametersOnConstraintId;
+
+	public function setUp() {
+		global $wgAPIModules;
+
+		$this->oldModuleDeclaration = $wgAPIModules['wbcheckconstraintparameters'];
+
+		$this->checkConstraintParametersOnPropertyId = function( $propertyId ) {
+			$this->assertTrue( false, 'checkConstraintParametersOnPropertyId method should not be called by this test.' );
+		};
+		$this->checkConstraintParametersOnConstraintId = function( $constraintId ) {
+			$this->assertTrue( false, 'checkConstraintParametersOnConstraintId method should not be called by this test.' );
+		};
+
+		$wgAPIModules['wbcheckconstraintparameters']['factory'] = function ( $main, $name, $prefix = '' ) {
+			$repo = WikibaseRepo::getDefaultInstance();
+			$apiHelperFactory = $repo->getApiHelperFactory( RequestContext::getMain() );
+			$statementGuidParser = $repo->getStatementGuidParser();
+
+			$delegatingConstraintChecker = $this->createMock( DelegatingConstraintChecker::class );
+			$delegatingConstraintChecker->method( 'checkConstraintParametersOnPropertyId' )
+				->will( $this->returnCallback(
+					function( $propertyId ) {
+						$callable = $this->checkConstraintParametersOnPropertyId;
+						return $callable( $propertyId );
+					}
+				) );
+			$delegatingConstraintChecker->method( 'checkConstraintParametersOnConstraintId' )
+				->will( $this->returnCallback(
+					function( $constraintId ) {
+						$callable = $this->checkConstraintParametersOnConstraintId;
+						return $callable( $constraintId );
+					}
+				) );
+
+			return new CheckConstraintParameters(
+				$main,
+				$name,
+				$prefix,
+				$apiHelperFactory,
+				$delegatingConstraintChecker,
+				$statementGuidParser
+			);
+		};
+
+		parent::setUp();
+	}
+
+	public function tearDown() {
+		global $wgAPIModules;
+
+		$wgAPIModules['wbcheckconstraintparameters'] = $this->oldModuleDeclaration;
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @param array $params
+	 * @return array wbcheckconstraintparameters response, slightly normalized
+	 */
+	private function doRequest( array $params ) {
+		$params['action'] = 'wbcheckconstraintparameters';
+
+		$result = $this->doApiRequest( $params, [], false, null )[0];
+
+		$this->assertSame( 1, $result['success'] );
+		$this->assertArrayHasKey( 'wbcheckconstraintparameters', $result );
+
+		ksort( $result['wbcheckconstraintparameters'] );
+		return $result['wbcheckconstraintparameters'];
+	}
+
+	public function testReportForNonexistentProperty() {
+		$this->checkConstraintParametersOnPropertyId = function( $propertyId ) {
+			return [];
+		};
+
+		$result = $this->doRequest(
+			[ CheckConstraintParameters::PARAM_PROPERTY_ID => self::P1 ]
+		);
+
+		$this->assertSame( [ self::P1 => [] ], $result );
+	}
+
+	public function testReportForNonexistentConstraint() {
+		$this->checkConstraintParametersOnConstraintId = function( $constraintId ) {
+			return null;
+		};
+
+		$result = $this->doRequest(
+			[ CheckConstraintParameters::PARAM_CONSTRAINT_ID => self::P1_NONEXISTENT ]
+		);
+
+		$this->assertSame(
+			[
+				self::P1 => [
+					self::P1_NONEXISTENT => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_NOT_FOUND
+					]
+				]
+			],
+			$result
+		);
+	}
+
+	public function testReportForGoodConstraint() {
+		$this->checkConstraintParametersOnConstraintId = function( $constraintId ) {
+			return [];
+		};
+
+		$result = $this->doRequest(
+			[
+				CheckConstraintParameters::PARAM_CONSTRAINT_ID => self::P1_GOOD
+			]
+		);
+
+		$this->assertSame(
+			[
+				self::P1 => [
+					self::P1_GOOD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => []
+					]
+				]
+			],
+			$result
+		);
+	}
+
+	public function testReportForBadConstraint() {
+		$this->checkConstraintParametersOnConstraintId = function( $constraintId ) {
+			return [ new ConstraintParameterException( 'test message' ) ];
+		};
+
+		$result = $this->doRequest(
+			[
+				CheckConstraintParameters::PARAM_CONSTRAINT_ID => self::P1_BAD
+			]
+		);
+
+		$this->assertSame(
+			[
+				self::P1 => [
+					self::P1_BAD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_NOT_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => [
+							[ CheckConstraintParameters::KEY_MESSAGE_HTML => 'test message' ]
+						]
+					]
+				]
+			],
+			$result
+		);
+	}
+
+	public function testReportForMultipleConstraints() {
+		$this->checkConstraintParametersOnConstraintId = function( $constraintId ) {
+			switch ( $constraintId ) {
+				case self::P1_NONEXISTENT:
+					return null;
+				case self::P1_GOOD:
+					return [];
+				case self::P1_BAD:
+					return [ new ConstraintParameterException( 'test message' ) ];
+			}
+		};
+
+		$result = $this->doRequest(
+			[
+				CheckConstraintParameters::PARAM_CONSTRAINT_ID =>
+					self::P1_NONEXISTENT . '|' .self::P1_GOOD . '|' .self::P1_BAD
+			]
+		);
+
+		$this->assertSame(
+			[
+				self::P1 => [
+					self::P1_NONEXISTENT => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_NOT_FOUND
+					],
+					self::P1_GOOD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => []
+					],
+					self::P1_BAD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_NOT_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => [
+							[ CheckConstraintParameters::KEY_MESSAGE_HTML => 'test message' ]
+						]
+					]
+				]
+			],
+			$result
+		);
+	}
+
+	public function testReportForMultipleProperties() {
+		$this->checkConstraintParametersOnPropertyId = function( $propertyId ) {
+			switch ( $propertyId->getSerialization() ) {
+				case self::P1:
+					return [
+						self::P1_GOOD => [],
+						self::P1_BAD => [ new ConstraintParameterException( 'test message' ) ]
+					];
+				case self::P2:
+					return [
+						self::P2_GOOD => []
+					];
+			}
+		};
+
+		$result = $this->doRequest(
+			[
+				CheckConstraintParameters::PARAM_PROPERTY_ID =>
+					self::P1 . '|' . self::P2
+			]
+		);
+
+		$this->assertSame(
+			[
+				self::P1 => [
+					self::P1_GOOD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => []
+					],
+					self::P1_BAD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_NOT_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => [
+							[ CheckConstraintParameters::KEY_MESSAGE_HTML => 'test message' ]
+						]
+					]
+				],
+				self::P2 => [
+					self::P2_GOOD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => []
+					]
+				]
+			],
+			$result
+		);
+	}
+
+	public function testReportForConstraintAndProperty() {
+		$this->checkConstraintParametersOnConstraintId = function( $constraintid ) {
+			return [ new ConstraintParameterException( 'test message' ) ];
+		};
+		$this->checkConstraintParametersOnPropertyId = function( $propertyId ) {
+			return [
+				self::P2_GOOD => []
+			];
+		};
+
+		$result = $this->doRequest(
+			[
+				CheckConstraintParameters::PARAM_CONSTRAINT_ID => self::P1_BAD,
+				CheckConstraintParameters::PARAM_PROPERTY_ID => self::P2
+			]
+		);
+
+		$this->assertSame(
+			[
+				self::P1 => [
+					self::P1_BAD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_NOT_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => [
+							[ CheckConstraintParameters::KEY_MESSAGE_HTML => 'test message' ]
+						]
+					]
+				],
+				self::P2 => [
+					self::P2_GOOD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => []
+					]
+				]
+			],
+			$result
+		);
+	}
+
+	public function testReportForConstraintAndPropertyOverlapping() {
+		$this->checkConstraintParametersOnPropertyId = function( $propertyId ) {
+			return [
+				self::P2_GOOD => []
+			];
+		};
+		// $this->checkConstraintParametersOnConstraintId not set:
+		// API should reuse result from property check
+
+		$result = $this->doRequest(
+			[
+				CheckConstraintParameters::PARAM_CONSTRAINT_ID => self::P2_GOOD,
+				CheckConstraintParameters::PARAM_PROPERTY_ID => self::P2
+			]
+		);
+
+		$this->assertSame(
+			[
+				self::P2 => [
+					self::P2_GOOD => [
+						CheckConstraintParameters::KEY_STATUS => CheckConstraintParameters::STATUS_OKAY,
+						CheckConstraintParameters::KEY_PROBLEMS => []
+					]
+				]
+			],
+			$result
+		);
+	}
+
+}
