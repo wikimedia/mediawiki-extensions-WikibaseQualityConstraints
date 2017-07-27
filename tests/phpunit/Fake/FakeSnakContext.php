2e160fc6982bead00b1d1155ab2e74b0c64b6f2d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-07-27 16:38:34 +0200

    Add FakeSnakContext test implementation
    This is a context for just a snak and optionally an entity (otherwise,
    the entity defaults to a new Q1 item), with no surrounding statement.
    It’s a useful minimal implementation for many tests.
    
    Change-Id: Ie4135663ea9a69c6166e82edf98a739fde5e039c

---
+++ tests/phpunit/Fake/FakeSnakContext.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\Tests\Fake;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Snak\Snak;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\AbstractContext;
+
+/**
+ * A constraint check context for a snak not connected to any statement.
+ * This is a minimal Context implementation for tests.
+ */
+class FakeSnakContext extends AbstractContext {
+
+	/**
+	 * @param Snak $snak
+	 * @param EntityDocument|null $entity defaults to a new Q1 item
+	 */
+	public function __construct(
+		Snak $snak,
+		EntityDocument $entity = null
+	) {
+		parent::__construct(
+			$entity ?: new Item( new ItemId( 'Q1' ) ),
+			$snak
+		);
+	}
+
+	public function getType() {
+		return 'test';
+	}
+
+	public function storeCheckResultInArray( array $result, array &$container ) {
+		$container[] = $result;
+	}
+
+}
