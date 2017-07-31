09f3a48513eb649fb7a9de6a01745f7024c2ec48
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-07-31 12:41:02 +0200

    Fix type checking SPARQL fallback
    The SPARQL fallback needs to happen at the outermost level, with the
    original entity, not on some inner nested entity. Consider the following
    subclass hierarchy:
    
                    v---------<
        Q1 -> Q2 -> Q3 -> Q4 -^
               \
                > Q5
    
    The PHP subtype checker (isSubclassOf()) will run through the Q3<->Q4
    cycle until it runs into the check limit. (This could of course be
    optimized, but that’s beside the point: there are enough hierarchies on
    Wikidata that exceed the limit without cycles.) Previously, it would
    then fall back to SPARQL, asking it if Q3 or Q4 are subclasses of the
    desired class (Q5). This is incorrect, since the entity we’re actually
    interested in is Q1.
    
    The solution to this is to perform the fallback at an outer level,
    outside of the recursive type check. To this end, a new method
    isSubclassOfWithSparqlFallback() is added, which performs this fallback,
    while isSubclassOf() throws an exception to indicate the fallback case
    instead of returning false. hasClassInRelation() and all the tests then
    use this new function instead of calling isSubclassOf() directly.
    
    Bug: T169326
    Change-Id: I2aaf3d6fa69791eff71718e808085cc99c128595

--- tests/phpunit/Checker/TypeChecker/TypeCheckerHelperTest.php
+++ tests/phpunit/Checker/TypeChecker/TypeCheckerHelperTest.php
@@ -4,12 +4,15 @@ namespace WikibaseQuality\ConstraintReport\Test\TypeChecker;
 
 use PHPUnit_Framework_TestCase;
 use Wikibase\DataModel\Services\Lookup\EntityLookup;
+use Wikibase\DataModel\Services\Lookup\InMemoryEntityLookup;
 use Wikibase\DataModel\Statement\Statement;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\Repo\Tests\NewItem;
+use Wikibase\Repo\Tests\NewStatement;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\SparqlHelper;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\TypeCheckerHelper;
 use WikibaseQuality\ConstraintReport\Tests\ConstraintParameters;
@@ -46,11 +49,14 @@ class TypeCheckerHelperTest extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
+	 * @param EntityLookup $lookup The backing lookup of the mock (defaults to JsonFileEntityLookup).
 	 * @return EntityLookup Expects that getEntity is called
 	 * exactly WBQualityConstraintsTypeCheckMaxEntities times.
 	 */
-	private function getMaxEntitiesLookup() {
-		$lookup = new JsonFileEntityLookup( __DIR__ );
+	private function getMaxEntitiesLookup( EntityLookup $lookup = null ) {
+		if ( $lookup === null ) {
+			$lookup = new JsonFileEntityLookup( __DIR__ );
+		}
 		$maxEntities = $this->getDefaultConfig()->get( 'WBQualityConstraintsTypeCheckMaxEntities' );
 
 		$spy = $this->getMock( EntityLookup::class );
@@ -63,15 +69,20 @@ class TypeCheckerHelperTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * @param boolean $return
+	 * @param array|null $arguments
 	 * @return SparqlHelper expects that {@link SparqlHelper::hasType}
 	 * is called exactly once and returns $return.
 	 */
-	private function getSparqlHelper( $return ) {
+	private function getSparqlHelper( $return, array $arguments = null ) {
+		if ( $arguments === null ) {
+			$arguments = [ $this->anything(), $this->anything(), $this->anything() ];
+		}
 		$mock = $this->getMockBuilder( SparqlHelper::class )
 			  ->disableOriginalConstructor()
 			  ->getMock();
 		$mock->expects( $this->once() )
 			->method( 'hasType' )
+			->withConsecutive( $arguments )
 			->willReturn( $return );
 		return $mock;
 	}
@@ -98,31 +109,81 @@ class TypeCheckerHelperTest extends PHPUnit_Framework_TestCase {
 	}
 
 	public function testCheckIsSubclassOfValidWithIndirection() {
-		$this->assertTrue( $this->getHelper()->isSubclassOf( new ItemId( 'Q6' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertTrue( $this->getHelper()->isSubclassOfWithSparqlFallback( new ItemId( 'Q6' ), [ 'Q100', 'Q101' ] ) );
 	}
 
 	public function testCheckIsSubclassOfInvalid() {
-		$this->assertFalse( $this->getHelper()->isSubclassOf( new ItemId( 'Q6' ), [ 'Q200', 'Q201' ] ) );
+		$this->assertFalse( $this->getHelper()->isSubclassOfWithSparqlFallback( new ItemId( 'Q6' ), [ 'Q200', 'Q201' ] ) );
 	}
 
 	public function testCheckIsSubclassCyclic() {
 		$helper = $this->getHelper( $this->getMaxEntitiesLookup() );
-		$this->assertFalse( $helper->isSubclassOf( new ItemId( 'Q7' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertFalse( $helper->isSubclassOfWithSparqlFallback( new ItemId( 'Q7' ), [ 'Q100', 'Q101' ] ) );
 	}
 
 	public function testCheckIsSubclassCyclicWide() {
 		$helper = $this->getHelper( $this->getMaxEntitiesLookup() );
-		$this->assertFalse( $helper->isSubclassOf( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertFalse( $helper->isSubclassOfWithSparqlFallback( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
 	}
 
 	public function testCheckIsSubclassCyclicWideWithSparqlTrue() {
 		$helper = $this->getHelper( $this->getMaxEntitiesLookup(), $this->getSparqlHelper( true ) );
-		$this->assertTrue( $helper->isSubclassOf( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertTrue( $helper->isSubclassOfWithSparqlFallback( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
 	}
 
 	public function testCheckIsSubclassCyclicWideWithSparqlFalse() {
 		$helper = $this->getHelper( $this->getMaxEntitiesLookup(), $this->getSparqlHelper( false ) );
-		$this->assertFalse( $helper->isSubclassOf( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
+		$this->assertFalse( $helper->isSubclassOfWithSparqlFallback( new ItemId( 'Q9' ), [ 'Q100', 'Q101' ] ) );
+	}
+
+	public function testCheckIsSubclassTree() {
+		$lookup = new InMemoryEntityLookup();
+		$subclassPid = $this->getDefaultConfig()->get( 'WBQualityConstraintsSubclassOfId' );
+
+		$q1 = NewItem::withId( 'Q1' )
+			->andStatement(
+				NewStatement::forProperty( $subclassPid )
+					->withValue( new ItemId( 'Q2' ) )
+			)
+			->build();
+		$lookup->addEntity( $q1 );
+		$q2 = NewItem::withId( 'Q2' )
+			->andStatement(
+				NewStatement::forProperty( $subclassPid )
+					->withValue( new ItemId( 'Q3' ) )
+			)
+			->andStatement(
+				NewStatement::forProperty( $subclassPid )
+					->withValue( new ItemId( 'Q5' ) )
+			)
+			->build();
+		$lookup->addEntity( $q2 );
+		$q3 = NewItem::withId( 'Q3' )
+			->andStatement(
+				NewStatement::forProperty( $subclassPid )
+					->withValue( new ItemId( 'Q4' ) )
+			)
+			->build();
+		$lookup->addEntity( $q3 );
+		$q4 = NewItem::withId( 'Q4' )
+			->andStatement(
+				NewStatement::forProperty( $subclassPid )
+					->withValue( new ItemId( 'Q3' ) )
+			)
+			->build();
+		$lookup->addEntity( $q4 );
+
+		$sparqlHelper = $this->getSparqlHelper(
+			true,
+			[
+				$this->identicalTo( 'Q1' ),
+				$this->identicalTo( [ 'Q5' ] ),
+				$this->identicalTo( false )
+			]
+		);
+		$helper = $this->getHelper( $this->getMaxEntitiesLookup( $lookup ), $sparqlHelper );
+
+		$this->assertTrue( $helper->isSubclassOfWithSparqlFallback( new ItemId( 'Q1' ), [ 'Q5' ] ) );
 	}
 
 }
