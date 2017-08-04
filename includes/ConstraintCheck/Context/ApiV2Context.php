49c8650fa49c5b4e8a789138d5eee8ed24cc7163
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-08-04 18:25:11 +0200

    Add MainSnakContext implementation
    This holds the same information as StatementSnak, but uses a different
    result layout that can also accommodate qualifier and reference
    constraint check results, as well as other information on a property.
    Most of the implementation of the layout is in an intermediate abstract
    class which can also be used by qualifier and reference contexts.
    
    Change-Id: I4e0808023787a2e606a669d20d62e6df77fad877

---
+++ includes/ConstraintCheck/Context/ApiV2Context.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\ConstraintCheck\Context;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Statement\Statement;
+
+/**
+ * Abstract superclass of all contexts for the second version of the API output format.
+ *
+ * This output format is modeled after the Wikibase entity JSON format,
+ * where an object with the members 'hash' and a list of 'reports' can appear
+ * wherever the entity JSON format contains snaks.
+ *
+ * That is, the container is keyed by entity ID and contains objects with a 'claims' member,
+ * which holds an object that is keyed by property ID and contains lists of statement objects,
+ * which have an 'id' member, a 'mainsnak' snak,
+ * 'qualifiers' keyed by property ID holding a list of snaks,
+ * and a list of 'references' each having a 'hash' and 'snaks'
+ * which are keyed by property ID and then hold a list of snaks.
+ */
+abstract class ApiV2Context extends AbstractContext {
+
+	/**
+	 * Returns the statement subcontainer.
+	 *
+	 * @param array &$container
+	 * @param string $entityId entity ID serialization
+	 * @param string $propertyId property ID serialization
+	 * @param string $statementId statement GUID
+	 * @return array
+	 */
+	protected function &getStatementArray(
+		array &$container,
+		$entityId,
+		$propertyId,
+		$statementId
+	) {
+		if ( !array_key_exists( $entityId, $container ) ) {
+			$container[$entityId] = [];
+		}
+		$entityContainer = &$container[$entityId];
+
+		if ( !array_key_exists( 'claims', $entityContainer ) ) {
+			$entityContainer['claims'] = [];
+		}
+		$claimsContainer = &$entityContainer['claims'];
+
+		if ( !array_key_exists( $propertyId, $claimsContainer ) ) {
+			$claimsContainer[$propertyId] = [];
+		}
+		$propertyContainer = &$claimsContainer[$propertyId];
+
+		foreach ( $propertyContainer as &$statement ) {
+			if ( $statement['id'] === $statementId ) {
+				$statementArray = &$statement;
+				break;
+			}
+		}
+		if ( !isset( $statementArray ) ) {
+			$statementArray = [ 'id' => $statementId ];
+			$propertyContainer[] = &$statementArray;
+		}
+
+		return $statementArray;
+	}
+
+	/**
+	 * This method returns the array with the 'hash' and 'reports' member.
+	 * It should locate the array in $container or,
+	 * if the array doesn’t exist yet, create it and emplace it there.
+	 *
+	 * @param array &$container
+	 * @return array
+	 */
+	abstract protected function &getMainArray( array &$container );
+
+	public function storeCheckResultInArray( array $result, array &$container ) {
+		$this->getMainArray( $container )['results'][] = $result;
+	}
+
+}
---
+++ includes/ConstraintCheck/Context/MainSnakContext.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\ConstraintCheck\Context;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Statement\Statement;
+
+/**
+ * A constraint check context for the main snak of a statement.
+ */
+class MainSnakContext extends ApiV2Context {
+
+	/**
+	 * @type Statement
+	 */
+	private $statement;
+
+	/**
+	 * @param EntityDocument $entity
+	 * @param Statement $statement
+	 */
+	public function __construct(
+		EntityDocument $entity,
+		Statement $statement
+	) {
+		parent::__construct( $entity, $statement->getMainSnak() );
+		$this->statement = $statement;
+	}
+
+	public function getType() {
+		return self::TYPE_STATEMENT;
+	}
+
+	public function getSnakRank() {
+		return $this->statement->getRank();
+	}
+
+	public function getSnakStatement() {
+		return $this->statement;
+	}
+
+	protected function &getMainArray( array &$container ) {
+		$statementArray = &$this->getStatementArray(
+			$container,
+			$this->entity->getId()->getSerialization(),
+			$this->statement->getPropertyId()->getSerialization(),
+			$this->statement->getGuid()
+		);
+
+		if ( !array_key_exists( 'mainsnak', $statementArray ) ) {
+			$statementArray['mainsnak'] = [ 'hash' => $this->statement->getMainSnak()->getHash() ];
+		}
+		$mainsnakArray = &$statementArray['mainsnak'];
+
+		return $mainsnakArray;
+	}
+
+}
