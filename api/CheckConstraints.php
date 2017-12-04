272d7adc74fc5262fae4074b5deda4ed3ecf8c3b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-12-04 15:16:36 +0100

    Add layer where constraint check results can be cached
    ResultsBuilder accepts a list of entity IDs, statement IDs, and
    constraint IDs to check, and returns an array structure containing
    constraint check results, which is exactly the level of abstraction
    where we want to cache constraint check results. The default
    implementation CheckingResultsBuilder contains code that is mostly
    copied from CheckConstraints (especially checkResultToArray from
    buildResult), with some cleanups. A future commit can add another
    implementation that does the caching.  CheckConstraints is now only a
    thin wrapper around ResultsBuilder, reduced to API-specific stuff like
    parameter parsing and validation.
    
    Tests for CheckingResultsBuilder are added in separate commits.
    
    Bug: T181080
    Change-Id: If5f93c6bf6da2149c9c89d657f96a1119e99e341

--- api/CheckConstraints.php
+++ api/CheckConstraints.php
@@ -4,29 +4,22 @@ namespace WikibaseQuality\ConstraintReport\Api;
 
 use ApiBase;
 use ApiMain;
-use ApiResult;
-use Config;
 use MediaWiki\MediaWikiServices;
 use RequestContext;
 use ValueFormatters\FormatterOptions;
 use Wikibase\Repo\ChangeOp\StatementChangeOpFactory;
 use Wikibase\DataModel\Entity\EntityIdParser;
 use Wikibase\DataModel\Entity\EntityIdParsingException;
-use Wikibase\DataModel\Entity\ItemId;
-use Wikibase\DataModel\Services\EntityId\EntityIdFormatter;
 use Wikibase\DataModel\Services\Statement\StatementGuidValidator;
 use Wikibase\Lib\SnakFormatter;
-use Wikibase\Lib\Store\EntityTitleLookup;
 use Wikibase\Repo\Api\ApiErrorReporter;
 use Wikibase\Repo\Api\ApiHelperFactory;
 use Wikibase\Repo\Api\ResultBuilder;
 use Wikibase\Repo\EntityIdLabelFormatterFactory;
 use Wikibase\Repo\WikibaseRepo;
-use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\Context;
-use WikibaseQuality\ConstraintReport\ConstraintCheck\DelegatingConstraintChecker;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintParameterParser;
-use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\CheckResult;
-use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\NullResult;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Api\ResultsBuilder;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Api\CheckingResultsBuilder;
 use WikibaseQuality\ConstraintReport\ConstraintParameterRenderer;
 use WikibaseQuality\ConstraintReport\ConstraintReportFactory;
 
@@ -53,11 +46,6 @@ class CheckConstraints extends ApiBase {
 	private $statementGuidValidator;
 
 	/**
-	 * @var DelegatingConstraintChecker
-	 */
-	private $delegatingConstraintChecker;
-
-	/**
 	 * @var ResultBuilder
 	 */
 	private $resultBuilder;
@@ -68,29 +56,9 @@ class CheckConstraints extends ApiBase {
 	private $errorReporter;
 
 	/**
-	 * @var StatementChangeOpFactory
+	 * @var ResultsBuilder
 	 */
-	private $statementChangeOpFactory;
-
-	/**
-	 * @var ConstraintParameterRenderer
-	 */
-	private $constraintParameterRenderer;
-
-	/**
-	 * @var EntityTitleLookup
-	 */
-	private $entityTitleLookup;
-
-	/**
-	 * @var EntityIdFormatter
-	 */
-	private $entityIdLabelFormatter;
-
-	/**
-	 * @var Config
-	 */
-	private $config;
+	private $resultsBuilder;
 
 	/**
 	 * Creates new instance from global state.
@@ -143,12 +111,14 @@ class CheckConstraints extends ApiBase {
 			$prefix,
 			$repo->getEntityIdParser(),
 			$repo->getStatementGuidValidator(),
-			$constraintReportFactory->getConstraintChecker(),
-			$constraintParameterRenderer,
 			$repo->getApiHelperFactory( RequestContext::getMain() ),
-			$repo->getEntityTitleLookup(),
-			$entityIdLabelFormatter,
-			$config
+			new CheckingResultsBuilder(
+				$constraintReportFactory->getConstraintChecker(),
+				$repo->getEntityTitleLookup(),
+				$entityIdLabelFormatter,
+				$constraintParameterRenderer,
+				$config
+			)
 		);
 	}
 
@@ -158,21 +128,13 @@ class CheckConstraints extends ApiBase {
 	 * @param string $prefix
 	 * @param EntityIdParser $entityIdParser
 	 * @param StatementGuidValidator $statementGuidValidator
-	 * @param DelegatingConstraintChecker $delegatingConstraintChecker
-	 * @param ConstraintParameterRenderer $constraintParameterRenderer
 	 * @param ApiHelperFactory $apiHelperFactory
-	 * @param EntityTitleLookup $entityTitleLookup
-	 * @param EntityIdFormatter $entityIdLabelFormatter
-	 * @param Config $config
+	 * @param ResultsBuilder $resultsBuilder
 	 */
 	public function __construct( ApiMain $main, $name, $prefix = '', EntityIdParser $entityIdParser,
-		StatementGuidValidator $statementGuidValidator,
-		DelegatingConstraintChecker $delegatingConstraintChecker,
-		ConstraintParameterRenderer $constraintParameterRenderer,
-		ApiHelperFactory $apiHelperFactory,
-		EntityTitleLookup $entityTitleLookup,
-		EntityIdFormatter $entityIdLabelFormatter,
-		Config $config
+								 StatementGuidValidator $statementGuidValidator,
+								 ApiHelperFactory $apiHelperFactory,
+								 ResultsBuilder $resultsBuilder
 	) {
 		parent::__construct( $main, $name, $prefix );
 
@@ -183,14 +145,9 @@ class CheckConstraints extends ApiBase {
 
 		$this->entityIdParser = $entityIdParser;
 		$this->statementGuidValidator = $statementGuidValidator;
-		$this->delegatingConstraintChecker = $delegatingConstraintChecker;
 		$this->resultBuilder = $apiHelperFactory->getResultBuilder( $this );
 		$this->errorReporter = $apiHelperFactory->getErrorReporter( $this );
-
-		$this->constraintParameterRenderer = $constraintParameterRenderer;
-		$this->entityTitleLookup = $entityTitleLookup;
-		$this->entityIdLabelFormatter = $entityIdLabelFormatter;
-		$this->config = $config;
+		$this->resultsBuilder = $resultsBuilder;
 	}
 
 	/**
@@ -201,57 +158,28 @@ class CheckConstraints extends ApiBase {
 			->increment( 'wikibase.quality.constraints.api.checkConstraints.execute' );
 
 		$params = $this->extractRequestParams();
-		$output = [];
 
 		$this->validateParameters( $params );
 		$entityIds = $this->parseEntityIds( $params );
 		$claimIds = $this->parseClaimIds( $params );
+		$constraintIDs = $params[self::PARAM_CONSTRAINT_ID];
 
-		$output = array_merge( $output, $this->checkItems( $entityIds, $params[self::PARAM_CONSTRAINT_ID] ) );
-		$output = array_merge( $output, $this->checkClaimIds( $claimIds, $params[self::PARAM_CONSTRAINT_ID] ) );
-
-		$this->getResult()->addValue( null, $this->getModuleName(), $this->buildResult( $output, $params[self::PARAM_ID] ) );
-		$this->resultBuilder->markSuccess( 1 );
-	}
-
-	public function defaultResults( Context $context ) {
-		return $context->getType() === Context::TYPE_STATEMENT ?
-			[ new NullResult( $context ) ] :
-			[];
-	}
-
-	private function checkItems( array $entityIds, $constraintIds ) {
-		$checkResults = [];
-
+		$this->getResult()->addValue(
+			null,
+			$this->getModuleName(),
+			$this->resultsBuilder->getResults(
+				$entityIds,
+				$claimIds, $constraintIDs
+			)
+		);
+		// ensure that result contains the given entity IDs even if they have no statements
 		foreach ( $entityIds as $entityId ) {
-			$currentCheckResults = $this->delegatingConstraintChecker->checkAgainstConstraintsOnEntityId(
-				$entityId,
-				$constraintIds,
-				[ $this, "defaultResults" ]
+			$this->getResult()->addArrayType(
+				[ $this->getModuleName(), $entityId->getSerialization() ],
+				'assoc'
 			);
-			if ( $currentCheckResults ) {
-				$checkResults = array_merge( $checkResults, $currentCheckResults );
-			}
 		}
-
-		return $checkResults;
-	}
-
-	private function checkClaimIds( array $claimIds, $constraintIds ) {
-		$checkResults = [];
-
-		foreach ( $claimIds as $claimId ) {
-			$currentCheckResults = $this->delegatingConstraintChecker->checkAgainstConstraintsOnClaimId(
-				$claimId,
-				$constraintIds,
-				[ $this, "defaultResults" ]
-			);
-			if ( $currentCheckResults ) {
-				$checkResults = array_merge( $checkResults, $currentCheckResults );
-			}
-		}
-
-		return $checkResults;
+		$this->resultBuilder->markSuccess( 1 );
 	}
 
 	private function parseEntityIds( array $params ) {
@@ -309,78 +237,6 @@ class CheckConstraints extends ApiBase {
 				"At least one of $paramId, $paramClaimId must be specified.", 'no-data' );
 		}
 		// contents of PARAM_ID and PARAM_CLAIM_ID are validated by parse{Entity,Claim}Ids()
-	}
-
-	/**
-	 * Converts a flat list of constraint check results
-	 * to a nested array structure which can be stored in the ApiResult.
-	 * The array is keyed by entity ID, then by property ID,
-	 * then by claim ID, and then contains a list of individual results:
-	 * { "Q1": { "P1": { "Q1$1a2b...": [ { "status": "compliance", ... }, { ... } ] } } }
-	 *
-	 * @param CheckResult[] $checkResults
-	 * @param string[]|null $entityIds optionally, a list of entity IDs that should be present
-	 * in the output even if there are no check results for them
-	 *
-	 * @return array
-	 */
-	private function buildResult( array $checkResults, $entityIds = null ) {
-		$constraintReport = [];
-		ApiResult::setArrayType( $constraintReport, 'assoc' );
-
-		// ensure that the report contains the given IDs even if there are no results for them
-		if ( $entityIds ) {
-			foreach ( $entityIds as $entityId ) {
-				$constraintReport[$entityId] = [];
-				ApiResult::setArrayType( $constraintReport[$entityId], 'assoc' );
-			}
-		}
-
-		foreach ( $checkResults as $checkResult ) {
-			if ( $checkResult instanceof NullResult ) {
-				$result = null;
-			} else {
-				$constraintId = $checkResult->getConstraint()->getConstraintId();
-				$typeItemId = $checkResult->getConstraint()->getConstraintTypeItemId();
-
-				$title = $this->entityTitleLookup->getTitleForId( $checkResult->getContext()->getSnak()->getPropertyId() );
-				$typeLabel = $this->entityIdLabelFormatter->formatEntityId( new ItemId( $typeItemId ) );
-				// TODO link to the statement when possible (T169224)
-				$link = $title->getFullUrl() . '#' . $this->config->get( 'WBQualityConstraintsPropertyConstraintId' );
-
-				$result = [
-					'status' => $checkResult->getStatus(),
-					'property' => $checkResult->getContext()->getSnak()->getPropertyId()->getSerialization(),
-					'constraint' => [
-						'id' => $checkResult->getConstraintId(),
-						'type' => $typeItemId,
-						'typeLabel' => $typeLabel,
-						'link' => $link,
-					]
-				];
-				if ( $this->config->get( 'WBQualityConstraintsIncludeDetailInApi' ) ) {
-					$result['constraint'] += [
-						'detail' => $checkResult->getParameters(),
-						'detailHTML' => $this->constraintParameterRenderer->formatParameters( $checkResult->getParameters() ),
-					];
-				}
-				if ( $checkResult->getMessage() ) {
-					$result['message-html'] = $checkResult->getMessage();
-				}
-				if ( $checkResult->getContext()->getType() === Context::TYPE_STATEMENT ) {
-					$result['claim'] = $checkResult->getContext()->getSnakStatement()->getGuid();
-				}
-				$cachingMetadata = $checkResult->getCachingMetadata();
-				if ( $cachingMetadata->isCached() ) {
-					$result['cached'] = [
-						'maximumAgeInSeconds' => $cachingMetadata->getMaximumAgeInSeconds(),
-					];
-				}
-			}
-
-			$checkResult->getContext()->storeCheckResultInArray( $result, $constraintReport );
-		}
-		return $constraintReport;
 	}
 
 	/**
