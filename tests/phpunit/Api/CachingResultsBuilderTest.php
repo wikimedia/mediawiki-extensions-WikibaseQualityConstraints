97745ae895faf6b9d45953659893870efab39115
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-12-11 12:20:43 +0100

    Add and use CachingResultsBuilder
    This ResultsBuilder implementation wraps another ResultsBuilder and
    stores its result in some cache, along with some information to be able
    to determine whether a cached result is fresh or not. For now, it does
    not retrieve the result from that cache – that will be implemented later
    (but that’s the reason why getAndStoreResults is a separate method and
    not inlined into getResults).
    
    CachingResultsBuilder is only used in the CheckConstraints API if the
    config setting WBQualityConstraintsCacheCheckConstraintsResults is
    enabled, which currently defaults to false, since it’s not very useful
    to store check results in the cache when there’s no code to read them
    from there yet.
    
    Bug: T182105
    Change-Id: I2fb05b91170eeb08571086fbb3bdc92f88a35015

---
+++ tests/phpunit/Api/CachingResultsBuilderTest.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\Tests\Api;
+
+use HashBagOStuff;
+use WANObjectCache;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\Store\EntityRevisionLookup;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Api\CachingResultsBuilder;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Api\ResultsBuilder;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Cache\CachedCheckConstraintsResponse;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Cache\CachingMetadata;
+
+/**
+ * @covers \WikibaseQuality\ConstraintReport\ConstraintCheck\Api\CachingResultsBuilder
+ *
+ * @license GNU GPL v2+
+ */
+class CachingResultsBuilderTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGetAndStoreResults_SameResults() {
+		$expectedResults = new CachedCheckConstraintsResponse(
+			[ 'Q100' => 'garbage data, should not matter' ],
+			CachingMetadata::fresh()
+		);
+		$q100 = new ItemId( 'Q100' );
+		$resultsBuilder = $this->getMock( ResultsBuilder::class );
+		$resultsBuilder->expects( $this->once() )
+			->method( 'getResults' )
+			->with( $this->equalTo( [ $q100 ] ), $this->equalTo( [] ), $this->equalTo( null ) )
+			->willReturn( $expectedResults );
+		$cachingResultsBuilder = new CachingResultsBuilder(
+			$resultsBuilder,
+			WANObjectCache::newEmpty(),
+			$this->getMock( EntityRevisionLookup::class ),
+			86400
+		);
+
+		$results = $cachingResultsBuilder->getAndStoreResults( [ $q100 ], [], null );
+
+		$this->assertSame( $expectedResults, $results );
+	}
+
+	public function testGetAndStoreResults_DontCacheClaimIds() {
+		$expectedResults = new CachedCheckConstraintsResponse(
+			[ 'Q100' => 'garbage data, should not matter' ],
+			CachingMetadata::fresh()
+		);
+		$resultsBuilder = $this->getMock( ResultsBuilder::class );
+		$resultsBuilder->expects( $this->once() )
+			->method( 'getResults' )
+			->with( $this->equalTo( [] ), $this->equalTo( [ 'fake' ] ), $this->equalTo( null ) )
+			->willReturn( $expectedResults );
+		$cache = $this->getMockBuilder( WANObjectCache::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$cache->expects( $this->never() )->method( 'set' );
+		$lookup = $this->getMock( EntityRevisionLookup::class );
+		$lookup->expects( $this->never() )->method( 'getLatestRevisionId ' );
+		$cachingResultsBuilder = new CachingResultsBuilder(
+			$resultsBuilder,
+			$cache,
+			$lookup,
+			86400
+		);
+
+		$cachingResultsBuilder->getAndStoreResults( [], [ 'fake' ], null );
+	}
+
+	public function testGetAndStoreResults_DontCacheWithConstraintIds() {
+		$expectedResults = new CachedCheckConstraintsResponse(
+			[ 'Q100' => 'garbage data, should not matter' ],
+			CachingMetadata::fresh()
+		);
+		$q100 = new ItemId( 'Q100' );
+		$resultsBuilder = $this->getMock( ResultsBuilder::class );
+		$resultsBuilder->expects( $this->once() )
+			->method( 'getResults' )
+			->with( $this->equalTo( [ $q100 ] ), $this->equalTo( [] ), $this->equalTo( [ 'fake' ] ) )
+			->willReturn( $expectedResults );
+		$cache = $this->getMockBuilder( WANObjectCache::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$cache->expects( $this->never() )->method( 'set' );
+		$lookup = $this->getMock( EntityRevisionLookup::class );
+		$lookup->expects( $this->never() )->method( 'getLatestRevisionId ' );
+		$cachingResultsBuilder = new CachingResultsBuilder(
+			$resultsBuilder,
+			$cache,
+			$lookup,
+			86400
+		);
+
+		$cachingResultsBuilder->getAndStoreResults( [ $q100 ], [], [ 'fake' ] );
+	}
+
+	public function testGetAndStoreResults_StoreResults() {
+		$expectedResults = new CachedCheckConstraintsResponse(
+			[ 'Q100' => 'garbage data, should not matter' ],
+			CachingMetadata::fresh()
+		);
+		$q100 = new ItemId( 'Q100' );
+		$resultsBuilder = $this->getMock( ResultsBuilder::class );
+		$resultsBuilder->expects( $this->once() )
+			->method( 'getResults' )
+			->with( $this->equalTo( [ $q100 ] ), $this->equalTo( [] ), $this->equalTo( null ) )
+			->willReturn( $expectedResults );
+		$cache = new WANObjectCache( [ 'cache' => new HashBagOStuff() ] );
+		$lookup = $this->getMock( EntityRevisionLookup::class );
+		$cachingResultsBuilder = new CachingResultsBuilder(
+			$resultsBuilder,
+			$cache,
+			$lookup,
+			86400
+		);
+
+		$cachingResultsBuilder->getAndStoreResults( [ $q100 ], [], null );
+		$cachedResults = $cache->get( $cache->makeKey(
+			'WikibaseQualityConstraints',
+			'checkConstraints',
+			'v2',
+			'Q100'
+		) );
+
+		$this->assertNotNull( $cachedResults );
+		$this->assertArrayHasKey( 'results', $cachedResults );
+		$this->assertSame( $expectedResults->getArray()['Q100'], $cachedResults['results'] );
+	}
+
+	public function testGetAndStoreResults_StoreLatestRevisionIds() {
+		$q100 = new ItemId( 'Q100' );
+		$q101 = new ItemId( 'Q101' );
+		$p102 = new PropertyId( 'P102' );
+		$expectedResults = new CachedCheckConstraintsResponse(
+			[ 'Q100' => 'garbage data, should not matter' ],
+			CachingMetadata::merge( [
+				CachingMetadata::ofEntityId( $q100 ),
+				CachingMetadata::ofEntityId( $q101 ),
+				CachingMetadata::ofEntityId( $p102 ),
+			] )
+		);
+		$revisionIds = [
+			$q100->getSerialization() => 12345,
+			$q101->getSerialization() => 1337,
+			$p102->getSerialization() => 42,
+		];
+		$resultsBuilder = $this->getMock( ResultsBuilder::class );
+		$resultsBuilder->expects( $this->once() )
+			->method( 'getResults' )
+			->with( $this->equalTo( [ $q100 ] ), $this->equalTo( [] ), $this->equalTo( null ) )
+			->willReturn( $expectedResults );
+		$cache = new WANObjectCache( [ 'cache' => new HashBagOStuff() ] );
+		$lookup = $this->getMock( EntityRevisionLookup::class );
+		$lookup->expects( $this->atLeast( 3 ) )
+			->method( 'getLatestRevisionId' )
+			->willReturnCallback( function( EntityId $entityId ) use ( $revisionIds ) {
+				$serialization = $entityId->getSerialization();
+				$this->assertArrayHasKey( $serialization, $revisionIds );
+				return $revisionIds[$serialization];
+			} );
+		$cachingResultsBuilder = new CachingResultsBuilder(
+			$resultsBuilder,
+			$cache,
+			$lookup,
+			86400
+		);
+
+		$cachingResultsBuilder->getAndStoreResults( [ $q100 ], [], null );
+		$cachedResults = $cache->get( $cache->makeKey(
+			'WikibaseQualityConstraints',
+			'checkConstraints',
+			'v2',
+			'Q100'
+		) );
+
+		$this->assertNotNull( $cachedResults );
+		$this->assertArrayHasKey( 'latestRevisionIds', $cachedResults );
+		$this->assertSame( $revisionIds, $cachedResults['latestRevisionIds'] );
+	}
+
+}
