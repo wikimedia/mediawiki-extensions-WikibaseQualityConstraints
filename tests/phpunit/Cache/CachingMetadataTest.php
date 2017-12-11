ad96677d6a2da62b719ee052f2f83836b0a8bb0b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-12-11 19:50:27 +0100

    Add tests for entity IDs in CachingMetadata
    I should have added these in commit e4171e0306 (change Id4ae2cfe09)…
    oh well.
    
    Change-Id: I748306aa12a624f26a8a419e0f53a62e9003b85c

--- tests/phpunit/Cache/CachingMetadataTest.php
+++ tests/phpunit/Cache/CachingMetadataTest.php
@@ -3,6 +3,8 @@
 namespace WikibaseQuality\ConstraintReport\Test\Cache;
 
 use InvalidArgumentException;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Cache\CachingMetadata;
 use Wikimedia\Assert\ParameterElementTypeException;
 use Wikimedia\Assert\ParameterTypeException;
@@ -49,17 +51,34 @@ class CachingMetadataTest extends \PHPUnit_Framework_TestCase {
 		CachingMetadata::ofMaximumAgeInSeconds( '42' );
 	}
 
+	public function testOfEntityId() {
+		$q42 = new ItemId( 'Q42' );
+		$cm = CachingMetadata::ofEntityId( $q42 );
+
+		$this->assertFalse( $cm->isCached() );
+		$this->assertSame( [ $q42 ], $cm->getDependedEntityIds() );
+	}
+
 	public function testMerge() {
+		$q42 = new ItemId( 'Q42' );
+		$p31 = new PropertyId( 'P31' );
 		$cm = CachingMetadata::merge( [
 			CachingMetadata::fresh(),
 			CachingMetadata::ofMaximumAgeInSeconds( 10 ),
 			CachingMetadata::ofMaximumAgeInSeconds( 42 ),
+			CachingMetadata::ofEntityId( $q42 ),
 			CachingMetadata::fresh(),
 			CachingMetadata::ofMaximumAgeInSeconds( 13 ),
+			CachingMetadata::ofEntityId( $p31 ),
 		] );
 
 		$this->assertTrue( $cm->isCached() );
 		$this->assertSame( 42, $cm->getMaximumAgeInSeconds() );
+		$expectedDependedEntityIds = [ $q42, $p31 ];
+		$actualDependedEntityIds = $cm->getDependedEntityIds();
+		sort( $expectedDependedEntityIds );
+		sort( $actualDependedEntityIds );
+		$this->assertSame( $expectedDependedEntityIds, $actualDependedEntityIds );
 	}
 
 	public function testMerge_fresh() {
