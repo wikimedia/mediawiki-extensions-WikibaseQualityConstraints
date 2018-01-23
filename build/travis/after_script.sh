7560b642d8a7aa5a420cb8d75a6ac988a2cea698
Amir Sarabadani <ladsgroup@gmail.com>
2018-01-23 20:32:45 +0100

    Workaround for coveralls bug #61
    See https://github.com/php-coveralls/php-coveralls/issues/61
    
    Change-Id: I3ecd95700616d5b684170d59d3409a9bd1d6ad92

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -2,4 +2,8 @@
 
 cd ../wiki/extensions/WikibaseQualityConstraints
 
+export CI_BUILD_NUMBER="$TRAVIS_BUILD_NUMBER"
+export CI_PULL_REQUEST="$TRAVIS_PULL_REQUEST"
+export CI_BRANCH="$TRAVIS_BRANCH"
+
 php vendor/bin/php-coveralls -v
