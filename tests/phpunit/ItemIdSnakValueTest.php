98a2d7e9e71880261129391262cb834d65ffb68b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-01-24 15:53:30 +0100

    Add tests for ItemIdSnakValue
    Change-Id: Ic3e84dfe44983a80499049472e3b62047223f7c9

---
+++ tests/phpunit/ItemIdSnakValueTest.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\Tests\ConstraintChecker;
+
+use DataValues\StringValue;
+use DomainException;
+use InvalidArgumentException;
+use Wikibase\DataModel\Entity\EntityIdValue;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Snak\PropertySomeValueSnak;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\ItemIdSnakValue;
+
+/**
+ * @covers WikibaseQuality\ConstraintReport\ConstraintCheck\ItemIdSnakValue
+ *
+ * @group WikibaseQualityConstraints
+ *
+ * @author Lucas Werkmeister
+ * @license GNU GPL v2+
+ */
+class ItemIdSnakValueTest extends \PHPUnit_Framework_TestCase {
+
+	public function testFromItemId() {
+		$itemId = new ItemId( 'Q1' );
+
+		$value = ItemIdSnakValue::fromItemId( $itemId );
+
+		$this->assertTrue( $value->isValue() );
+		$this->assertFalse( $value->isSomeValue() );
+		$this->assertFalse( $value->isNoValue() );
+		$this->assertSame( $itemId, $value->getItemId() );
+	}
+
+	public function testSomeValue() {
+		$value = ItemIdSnakValue::someValue();
+
+		$this->assertFalse( $value->isValue() );
+		$this->assertTrue( $value->isSomeValue() );
+		$this->assertFalse( $value->isNoValue() );
+
+		$this->setExpectedException( DomainException::class );
+		$value->getItemId();
+	}
+
+	public function testNoValue() {
+		$value = ItemIdSnakValue::noValue();
+
+		$this->assertFalse( $value->isValue() );
+		$this->assertFalse( $value->isSomeValue() );
+		$this->assertTrue( $value->isNoValue() );
+
+		$this->setExpectedException( DomainException::class );
+		$value->getItemId();
+	}
+
+	public function testFromSnak_ItemId() {
+		$itemId = new ItemId( 'Q1' );
+		$snak = new PropertyValueSnak(
+			new PropertyId( 'P100' ),
+			new EntityIdValue( $itemId )
+		);
+
+		$value = ItemIdSnakValue::fromSnak( $snak );
+
+		$this->assertTrue( $value->isValue() );
+		$this->assertSame( $itemId, $value->getItemId() );
+	}
+
+	public function testFromSnak_PropertyId() {
+		$propertyId = new PropertyId( 'P1' );
+		$snak = new PropertyValueSnak(
+			new PropertyId( 'P100' ),
+			new EntityIdValue( $propertyId )
+		);
+
+		$this->setExpectedException( InvalidArgumentException::class );
+		$value = ItemIdSnakValue::fromSnak( $snak );
+	}
+
+	public function testFromSnak_String() {
+		$snak = new PropertyValueSnak(
+			new PropertyId( 'P100' ),
+			new StringValue( 'Q1' )
+		);
+
+		$this->setExpectedException( InvalidArgumentException::class );
+		$value = ItemIdSnakValue::fromSnak( $snak );
+	}
+
+	public function testFromSnak_SomeValue() {
+		$snak = new PropertySomeValueSnak( new PropertyId( 'P100' ) );
+
+		$value = ItemIdSnakValue::fromSnak( $snak );
+
+		$this->assertTrue( $value->isSomeValue() );
+	}
+
+	public function testFromSnak_NoValue() {
+		$snak = new PropertyNoValueSnak( new PropertyId( 'P100' ) );
+
+		$value = ItemIdSnakValue::fromSnak( $snak );
+
+		$this->assertTrue( $value->isNoValue() );
+	}
+
+	public function testMatchesSnak_ItemId() {
+		$itemId = new ItemId( 'Q1' );
+		$snak = new PropertyValueSnak(
+			new PropertyId( 'P100' ),
+			new EntityIdValue( $itemId )
+		);
+
+		$this->assertTrue( ItemIdSnakValue::fromItemId( $itemId )->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::someValue()->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::noValue()->matchesSnak( $snak ) );
+	}
+
+	public function testMatchesSnak_PropertyId() {
+		$itemId = new ItemId( 'Q1' );
+		$propertyId = new PropertyId( 'P1' );
+		$snak = new PropertyValueSnak(
+			new PropertyId( 'P100' ),
+			new EntityIdValue( $propertyId )
+		);
+
+		$this->assertFalse( ItemIdSnakValue::fromItemId( $itemId )->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::someValue()->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::noValue()->matchesSnak( $snak ) );
+	}
+
+	public function testMatchesSnak_String() {
+		$itemId = new ItemId( 'Q1' );
+		$propertyId = new PropertyId( 'P1' );
+		$snak = new PropertyValueSnak(
+			new PropertyId( 'P100' ),
+			new StringValue( 'Q1' )
+		);
+
+		$this->assertFalse( ItemIdSnakValue::fromItemId( $itemId )->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::someValue()->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::noValue()->matchesSnak( $snak ) );
+	}
+
+	public function testMatchesSnak_SomeValue() {
+		$itemId = new ItemId( 'Q1' );
+		$snak = new PropertySomeValueSnak( new PropertyId( 'P100' ) );
+
+		$this->assertFalse( ItemIdSnakValue::fromItemId( $itemId )->matchesSnak( $snak ) );
+		$this->assertTrue( ItemIdSnakValue::someValue()->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::noValue()->matchesSnak( $snak ) );
+	}
+
+	public function testMatchesSnak_NoValue() {
+		$itemId = new ItemId( 'Q1' );
+		$snak = new PropertyNoValueSnak( new PropertyId( 'P100' ) );
+
+		$this->assertFalse( ItemIdSnakValue::fromItemId( $itemId )->matchesSnak( $snak ) );
+		$this->assertFalse( ItemIdSnakValue::someValue()->matchesSnak( $snak ) );
+		$this->assertTrue( ItemIdSnakValue::noValue()->matchesSnak( $snak ) );
+	}
+
+}
