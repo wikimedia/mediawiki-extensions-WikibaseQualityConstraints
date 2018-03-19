2f563c3000aced2fd306685d9ff390233193bc79
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-03-19 18:04:14 +0100

    Warn when loading extension via PHP entry point
    Now that this extension is loaded via extension registration in
    production (since change I28ac34accc), I don’t think we need to keep
    this warning commented out anymore.
    
    Change-Id: I53f2daa0146e6f440fb0dcb29aae3dbb0cfb5171

--- WikibaseQualityConstraints.php
+++ WikibaseQualityConstraints.php
@@ -9,11 +9,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['WikibaseQualityConstraints'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['WikibaseQualityConstraintsAlias'] = __DIR__ . '/WikibaseQualityConstraints.alias.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for WikibaseQualityConstraints extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the WikibaseQualityConstraints extension requires MediaWiki 1.25+' );
