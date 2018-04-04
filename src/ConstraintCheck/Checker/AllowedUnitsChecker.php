50e01839f235e033c0487bb8f3fcd5cd43a3c69d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-04-04 18:43:29 +0200

    Add “allowed units” constraint type
    This constraint type restricts the allowed units for a quantity value.
    Unit conversion is attempted on both the value and the allowed units of
    the constraint, to minimize the number of violation reports for now
    (though this might change later).
    
    Bug: T164372
    Change-Id: I8b0060d35b5f56fbaeda30e0e8220dc541da1edf
    Depends-On: I8ea741aeb383df3f92f7165772ba0ac9e2c54777
    Depends-On: I0b176594537e7c5b30b4d7c6c5bf2adc21093e0a

---
+++ src/ConstraintCheck/Checker/AllowedUnitsChecker.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\ConstraintCheck\Checker;
+
+use DataValues\UnboundedQuantityValue;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\Lib\Units\UnitConverter;
+use WikibaseQuality\ConstraintReport\Constraint;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\ConstraintChecker;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\Context;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintParameterException;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintParameterParser;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\UnitsParameter;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Message\ViolationMessage;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\CheckResult;
+use WikibaseQuality\ConstraintReport\Role;
+
+/**
+ * @author Lucas Werkmeister
+ * @license GPL-2.0-or-later
+ */
+class AllowedUnitsChecker implements ConstraintChecker {
+
+	/**
+	 * @var ConstraintParameterParser
+	 */
+	private $constraintParameterParser;
+
+	/**
+	 * @var UnitConverter|null
+	 */
+	private $unitConverter;
+
+	/**
+	 * @param ConstraintParameterParser $constraintParameterParser
+	 * @param UnitConverter|null $unitConverter
+	 */
+	public function __construct(
+		ConstraintParameterParser $constraintParameterParser,
+		UnitConverter $unitConverter = null
+	) {
+		$this->constraintParameterParser = $constraintParameterParser;
+		$this->unitConverter = $unitConverter;
+	}
+
+	/**
+	 * @codeCoverageIgnore This method is purely declarative.
+	 */
+	public function getSupportedContextTypes() {
+		return [
+			Context::TYPE_STATEMENT => CheckResult::STATUS_COMPLIANCE,
+			Context::TYPE_QUALIFIER => CheckResult::STATUS_COMPLIANCE,
+			Context::TYPE_REFERENCE => CheckResult::STATUS_COMPLIANCE,
+		];
+	}
+
+	/**
+	 * @codeCoverageIgnore This method is purely declarative.
+	 */
+	public function getDefaultContextTypes() {
+		return [
+			Context::TYPE_STATEMENT,
+			Context::TYPE_QUALIFIER,
+			Context::TYPE_REFERENCE,
+		];
+	}
+
+	/**
+	 * Checks an “allowed units” constraint.
+	 *
+	 * @param Context $context
+	 * @param Constraint $constraint
+	 *
+	 * @throws ConstraintParameterException
+	 * @return CheckResult
+	 */
+	public function checkConstraint( Context $context, Constraint $constraint ) {
+		$parameters = [];
+		$constraintParameters = $constraint->getConstraintParameters();
+		$unitsParameter = $this->constraintParameterParser
+			->parseUnitsParameter(
+				$constraintParameters,
+				$constraint->getConstraintTypeItemId()
+			);
+
+		$snak = $context->getSnak();
+		if ( !$snak instanceof PropertyValueSnak ) {
+			// nothing to check
+			return new CheckResult( $context, $constraint, $parameters, CheckResult::STATUS_COMPLIANCE );
+		}
+
+		$dataValue = $snak->getDataValue();
+		if ( !$dataValue instanceof UnboundedQuantityValue ) {
+			$message = ( new ViolationMessage( 'wbqc-violation-message-value-needed-of-type' ) )
+				->withEntityId( new ItemId( $constraint->getConstraintTypeItemId() ), Role::CONSTRAINT_TYPE_ITEM )
+				->withDataValueType( 'quantity' );
+			return new CheckResult( $context, $constraint, $parameters, CheckResult::STATUS_VIOLATION, $message );
+		}
+
+		if ( $dataValue->getUnit() === '1' ) {
+			return $this->checkUnitless( $context, $constraint, $unitsParameter, $snak );
+		}
+
+		$status = CheckResult::STATUS_VIOLATION;
+		$actualUnit = $this->standardize( $dataValue )->getUnit();
+		foreach ( $unitsParameter->getUnitQuantities() as $unitQuantity ) {
+			$allowedUnit = $this->standardize( $unitQuantity )->getUnit();
+			if ( $actualUnit === $allowedUnit ) {
+				$status = CheckResult::STATUS_COMPLIANCE;
+				break;
+			}
+		}
+
+		if ( $status === CheckResult::STATUS_VIOLATION ) {
+			$messageKey = $unitsParameter->getUnitlessAllowed() ?
+				'wbqc-violation-message-units-or-none' :
+				'wbqc-violation-message-units';
+			$message = ( new ViolationMessage( $messageKey ) )
+				->withEntityId( $snak->getPropertyId() )
+				->withEntityIdList( $unitsParameter->getUnitItemIds() );
+		} else {
+			$message = null;
+		}
+
+		return new CheckResult( $context, $constraint, $parameters, $status, $message );
+	}
+
+	/**
+	 * @param Context $context
+	 * @param Constraint $constraint
+	 * @param UnitsParameter $unitsParameter
+	 * @param PropertyValueSnak $snak
+	 * @return CheckResult
+	 */
+	private function checkUnitless(
+		Context $context,
+		Constraint $constraint,
+		UnitsParameter $unitsParameter,
+		PropertyValueSnak $snak
+	) {
+		if ( $unitsParameter->getUnitlessAllowed() ) {
+			$message = null;
+			$status = CheckResult::STATUS_COMPLIANCE;
+		} else {
+			$message = ( new ViolationMessage( 'wbqc-violation-message-units' ) )
+				->withEntityId( $snak->getPropertyId() )
+				->withEntityIdList( $unitsParameter->getUnitItemIds() );
+			$status = CheckResult::STATUS_VIOLATION;
+		}
+
+		return new CheckResult( $context, $constraint, [], $status, $message );
+	}
+
+	/**
+	 * Convert $value to standard units.
+	 *
+	 * @param UnboundedQuantityValue $value
+	 * @return UnboundedQuantityValue
+	 */
+	private function standardize( UnboundedQuantityValue $value ) {
+		if ( $this->unitConverter === null ) {
+			return $value;
+		}
+
+		$standard = $this->unitConverter->toStandardUnits( $value );
+		if ( $standard !== null ) {
+			return $standard;
+		} else {
+			return $value;
+		}
+	}
+
+	public function checkConstraintParameters( Constraint $constraint ) {
+		$constraintParameters = $constraint->getConstraintParameters();
+		$exceptions = [];
+		try {
+			$this->constraintParameterParser->parseItemsParameter(
+				$constraintParameters,
+				$constraint->getConstraintTypeItemId(),
+				true
+			);
+		} catch ( ConstraintParameterException $e ) {
+			$exceptions[] = $e;
+		}
+		return $exceptions;
+	}
+
+}
