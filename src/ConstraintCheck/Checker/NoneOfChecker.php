d8c6053a2f40cacd3c2005b7683bf6c7df39cda7
Amir Sarabadani <ladsgroup@gmail.com>
2018-05-03 18:24:09 +0200

    Add 'none of' constraint
    Bug: T183092
    Change-Id: I033a033591ecf04fcf2220714833f448d8da9077

---
+++ src/ConstraintCheck/Checker/NoneOfChecker.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\ConstraintCheck\Checker;
+
+use WikibaseQuality\ConstraintReport\Constraint;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\ConstraintChecker;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\Context;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintParameterException;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Helper\ConstraintParameterParser;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Message\ViolationMessage;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\CheckResult;
+use WikibaseQuality\ConstraintReport\ConstraintParameterRenderer;
+use WikibaseQuality\ConstraintReport\Role;
+use Wikibase\DataModel\Statement\Statement;
+
+/**
+ * @author Amir Sarabadani
+ * @license GPL-2.0-or-later
+ */
+class NoneOfChecker implements ConstraintChecker {
+
+	/**
+	 * @var ConstraintParameterParser
+	 */
+	private $constraintParameterParser;
+
+	/**
+	 * @var ConstraintParameterRenderer
+	 */
+	private $constraintParameterRenderer;
+
+	public function __construct(
+		ConstraintParameterParser $constraintParameterParser,
+		ConstraintParameterRenderer $constraintParameterRenderer
+	) {
+		$this->constraintParameterParser = $constraintParameterParser;
+		$this->constraintParameterRenderer = $constraintParameterRenderer;
+	}
+
+	/**
+	 * @codeCoverageIgnore This method is purely declarative.
+	 */
+	public function getSupportedContextTypes() {
+		return [
+			Context::TYPE_STATEMENT => CheckResult::STATUS_COMPLIANCE,
+			Context::TYPE_QUALIFIER => CheckResult::STATUS_COMPLIANCE,
+			Context::TYPE_REFERENCE => CheckResult::STATUS_COMPLIANCE,
+		];
+	}
+
+	/**
+	 * @codeCoverageIgnore This method is purely declarative.
+	 */
+	public function getDefaultContextTypes() {
+		return [
+			Context::TYPE_STATEMENT,
+			Context::TYPE_QUALIFIER,
+			Context::TYPE_REFERENCE,
+		];
+	}
+
+	/**
+	 * Checks 'None of' constraint.
+	 *
+	 * @param Context $context
+	 * @param Constraint $constraint
+	 *
+	 * @throws ConstraintParameterException
+	 * @return CheckResult
+	 */
+	public function checkConstraint( Context $context, Constraint $constraint ) {
+		if ( $context->getSnakRank() === Statement::RANK_DEPRECATED ) {
+			return new CheckResult( $context, $constraint, [], CheckResult::STATUS_DEPRECATED );
+		}
+
+		$parameters = [];
+		$constraintParameters = $constraint->getConstraintParameters();
+
+		$items = $this->constraintParameterParser->parseItemsParameter( $constraintParameters, $constraint->getConstraintTypeItemId(), true );
+		$parameters['item'] = $items;
+
+		$snak = $context->getSnak();
+
+		$message = null;
+		$status = CheckResult::STATUS_COMPLIANCE;
+
+		foreach ( $items as $item ) {
+			if ( $item->matchesSnak( $snak ) ) {
+				$message = ( new ViolationMessage( 'wbqc-violation-message-none-of' ) )
+					->withEntityId( $context->getSnak()->getPropertyId(), Role::PREDICATE )
+					->withItemIdSnakValueList( $items, Role::OBJECT );
+				$status = CheckResult::STATUS_VIOLATION;
+				break;
+			}
+		}
+
+		return new CheckResult( $context, $constraint, $parameters, $status, $message );
+	}
+
+	public function checkConstraintParameters( Constraint $constraint ) {
+		$constraintParameters = $constraint->getConstraintParameters();
+		$exceptions = [];
+		try {
+			$this->constraintParameterParser->parseItemsParameter( $constraintParameters, $constraint->getConstraintTypeItemId(), true );
+		} catch ( ConstraintParameterException $e ) {
+			$exceptions[] = $e;
+		}
+		return $exceptions;
+	}
+
+}
