2c41d3a406095fbff30231a17789766737b7cfbd
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-06-19 14:41:51 +0200

    Add “property scope” constraint type
    This is a more general version of the “used as main value”, “used as
    qualifier” and “used as reference” constraint types, and will eventually
    replace them (though for some time we support both). It can express all
    of those constraint types, but also combinations like “used as main
    value or qualifier but not as reference” which previously were only
    possible with ugly constructs abusing the constraint scope.
    
    Bug: T197473
    Change-Id: I73deee88deb6a82026252209903e6ff682f959b3

---
+++ tests/phpunit/Checker/PropertyScopeCheckerTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace WikibaseQuality\ConstraintReport\Tests\Checker;
+
+use Wikibase\DataModel\Reference;
+use Wikibase\Repo\Tests\NewItem;
+use Wikibase\Repo\Tests\NewStatement;
+use WikibaseQuality\ConstraintReport\Constraint;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Checker\PropertyScopeChecker;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\Context;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\MainSnakContext;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\QualifierContext;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Context\ReferenceContext;
+use WikibaseQuality\ConstraintReport\Tests\ConstraintParameters;
+use WikibaseQuality\ConstraintReport\Tests\ResultAssertions;
+
+/**
+ * @covers WikibaseQuality\ConstraintReport\ConstraintCheck\Checker\PropertyScopeChecker
+ *
+ * @group WikibaseQualityConstraints
+ *
+ * @author Lucas Werkmeister
+ * @license GPL-2.0-or-later
+ */
+class PropertyScopeCheckerTest extends \PHPUnit\Framework\TestCase {
+	use ResultAssertions, ConstraintParameters;
+
+	/**
+	 * @param Context $context
+	 * @param string[] $contextTypes
+	 * @param bool $ok
+	 * @dataProvider provideContextsAndContextTypes
+	 */
+	public function testPropertyScopeConstraint( Context $context, array $contextTypes, $ok ) {
+		$checker = new PropertyScopeChecker( $this->getConstraintParameterParser() );
+		$constraint = $this->getConstraintMock( $this->propertyScopeParameter( $contextTypes ) );
+
+		$result = $checker->checkConstraint( $context, $constraint );
+
+		if ( $ok ) {
+			$this->assertCompliance( $result );
+		} else {
+			$this->assertViolation( $result, 'wbqc-violation-message-property-scope' );
+		}
+	}
+
+	public function provideContextsAndContextTypes() {
+		$statement = NewStatement::noValueFor( 'P1' )
+			->build();
+		$snak = $statement->getMainSnak();
+		$item = NewItem::withId( 'Q1' )
+			->andStatement( $statement )
+			->build();
+		$statementContext = new MainSnakContext( $item, $statement );
+		$qualifierContext = new QualifierContext( $item, $statement, $snak );
+		$referenceContext = new ReferenceContext( $item, $statement, new Reference(), $snak );
+		$deprecatedStatement = NewStatement::noValueFor( 'P1' )
+			->withDeprecatedRank()
+			->build();
+		$deprecatedContext = new MainSnakContext( $item, $deprecatedStatement );
+
+		return [
+			[ $statementContext, [ Context::TYPE_STATEMENT ], true ],
+			[ $statementContext, [ Context::TYPE_STATEMENT, Context::TYPE_QUALIFIER ], true ],
+			[ $statementContext, [ Context::TYPE_STATEMENT, Context::TYPE_REFERENCE ], true ],
+			[ $statementContext, [ Context::TYPE_QUALIFIER, Context::TYPE_REFERENCE ], false ],
+			[ $qualifierContext, [ Context::TYPE_QUALIFIER ], true ],
+			[ $referenceContext, [ Context::TYPE_REFERENCE ], true ],
+			[ $qualifierContext, [ Context::TYPE_REFERENCE ], false ],
+			[ $referenceContext, [ Context::TYPE_QUALIFIER ], false ],
+			[ $deprecatedContext, [ Context::TYPE_QUALIFIER ], false ],
+		];
+	}
+
+	public function testCheckConstraintParameters() {
+		$checker = new PropertyScopeChecker( $this->getConstraintParameterParser() );
+		$constraint = $this->getConstraintMock( [] );
+
+		$result = $checker->checkConstraintParameters( $constraint );
+
+		$this->assertCount( 1, $result );
+	}
+
+	/**
+	 * @param string[] $parameters
+	 *
+	 * @return Constraint
+	 */
+	private function getConstraintMock( array $parameters ) {
+		$mock = $this
+			->getMockBuilder( Constraint::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$mock->expects( $this->any() )
+			->method( 'getConstraintParameters' )
+			->will( $this->returnValue( $parameters ) );
+		$mock->expects( $this->any() )
+			->method( 'getConstraintTypeItemId' )
+			->will( $this->returnValue( 'Q53869507' ) );
+
+		return $mock;
+	}
+
+}
