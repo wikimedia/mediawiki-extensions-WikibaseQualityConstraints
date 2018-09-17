ae6d129bfb81ff22c60c027b593de092c54990c0
Pablo Grass <pablo.grass@wikimedia.de>
2018-09-17 12:22:17 +0200

    gadget: abilities as methods
    Make the gadget's abilities individually invokable methods.
    
    Bug: T195828
    Change-Id: I8eaf718e9017324b7c714511f4abda62f06069a2

--- modules/gadget.js
+++ modules/gadget.js
@@ -3,6 +3,95 @@ module.exports = ( function ( mw, wb, $, OO ) {
 
 	var CACHED_STATUSES = 'violation|warning|bad-parameters';
 
+	function SELF() {}
+
+	SELF.prototype.defaultBehavior = function () {
+		var entityId = mw.config.get( 'wbEntityId' );
+
+		if ( entityId === null || mw.config.get( 'wgMFMode' ) || !mw.config.get( 'wbIsEditView' ) ) {
+			// no entity, mobile frontend, or not editing (diff, oldid, …) – skip
+			return;
+		}
+
+		mw.loader.using( [
+			'mediawiki.api',
+			'oojs-ui-core',
+			'oojs-ui-widgets',
+			'oojs-ui.styles.icons-alerts',
+			'wikibase.quality.constraints.icon',
+			'wikibase.quality.constraints.ui'
+		] ).done( function () {
+			var api = new mw.Api( this._mwApiOptions() ),
+				lang = mw.config.get( 'wgUserLanguage' );
+
+			this.fullCheck( api, lang, entityId );
+
+			if ( mw.config.get( 'wgPageContentModel' ) === 'wikibase-property' ) {
+				this.propertyParameterCheck( api, lang, entityId );
+			}
+
+			mw.hook( 'wikibase.statement.saved' ).add( function ( entityId, statementId ) {
+				mw.track( 'counter.MediaWiki.wikibase.quality.constraints.gadget.saveStatement' );
+				this.snakCheck( api, lang, entityId, statementId );
+			}.bind( this ) );
+		}.bind( this ) );
+	};
+
+	SELF.prototype.fullCheck = function ( api, lang, entityId ) {
+		var self = this;
+		mw.track( 'counter.MediaWiki.wikibase.quality.constraints.gadget.loadEntity' );
+		return api.get( {
+			action: 'wbcheckconstraints',
+			format: 'json',
+			formatversion: 2,
+			uselang: lang,
+			id: entityId,
+			status: CACHED_STATUSES
+		} ).then( function ( data ) {
+			$( '.wbqc-reports-button' ).remove();
+			$( '.wikibase-statementgroupview .wikibase-statementview' )
+				.each( function () { self._addReportsToStatement( data.wbcheckconstraints[ entityId ], $( this ) ); } );
+		} );
+	};
+
+	SELF.prototype.snakCheck = function ( api, lang, entityId, statementId ) {
+		var isUpdated = false,
+			statementClass = 'wikibase-statement-' + statementId.replace( /\$/, '\\$$' ),
+			self = this;
+
+		api.get( {
+			action: 'wbcheckconstraints',
+			format: 'json',
+			formatversion: 2,
+			uselang: lang,
+			claimid: statementId,
+			status: CACHED_STATUSES
+		} ).then( function ( data ) {
+			if ( isUpdated ) {
+				return;
+			}
+
+			$( '.wikibase-statementgroupview .wikibase-statementview.' + statementClass )
+				.each( function () { self._addReportsToStatement( data.wbcheckconstraints[ entityId ], $( this ) ); } );
+		} );
+		this.fullCheck( api, lang, entityId ).then( function () {
+			isUpdated = true;
+		} );
+	};
+
+	SELF.prototype.propertyParameterCheck = function ( api, lang, entityId ) {
+		mw.track( 'counter.MediaWiki.wikibase.quality.constraints.gadget.loadProperty' );
+		return api.get( {
+			action: 'wbcheckconstraintparameters',
+			format: 'json',
+			formatversion: 2,
+			uselang: lang,
+			propertyid: entityId
+		} ).then( function ( data ) {
+			this._addParameterReports( data.wbcheckconstraintparameters[ entityId ] );
+		}.bind( this ) );
+	};
+
 	/**
 	 * Create a popup button according to the parameters and append it to the container.
 	 *
@@ -12,7 +101,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 	 * @param {string} iconTitleMessageKey The message key of the title attribute of the icon.
 	 * @param {string} [flags] Optional custom flags the {@link OO.ui.PopupButtonWidget} can understand.
 	 */
-	function buildPopup( $content, $container, icon, iconTitleMessageKey, flags /* = '' */ ) {
+	SELF.prototype._buildPopup = function ( $content, $container, icon, iconTitleMessageKey, flags /* = '' */ ) {
 		var widget = new OO.ui.PopupButtonWidget( {
 			icon: icon,
 			iconTitle: mw.message( iconTitleMessageKey ).text(),
@@ -31,7 +120,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 		widget.popup.$element.css( 'z-index', 2 ); // prevent collision with rank selector and property grey field
 
 		$container.append( widget.$element );
-	}
+	};
 
 	/**
 	 * Get a message indicating the given cached status.
@@ -40,7 +129,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 	 * Typically an object with a `maximumAgeInSeconds` member.
 	 * @returns {string} HTML
 	 */
-	function getCachedMessage( cached ) {
+	SELF.prototype._getCachedMessage = function ( cached ) {
 		var maximumAgeInMinutes,
 			maximumAgeInHours,
 			maximumAgeInDays;
@@ -65,7 +154,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 			return mw.message( 'wbqc-cached-generic' )
 				.escaped();
 		}
-	}
+	};
 
 	/**
 	 * Build a panel for a single constraint check result.
@@ -73,26 +162,26 @@ module.exports = ( function ( mw, wb, $, OO ) {
 	 * @param {Object} result The constraint check result.
 	 * @returns {wikibase.quality.constraints.ui.ConstraintReportPanel}
 	 */
-	function buildReport( result ) {
+	SELF.prototype._buildReport = function ( result ) {
 		var config = {
 			status: result.status,
 			constraint: result.constraint,
 			message: result[ 'message-html' ]
 		};
 		if ( result.cached ) {
-			config.ancillaryMessages = [ getCachedMessage( result.cached ) ];
+			config.ancillaryMessages = [ this._getCachedMessage( result.cached ) ];
 		}
 		return new wb.quality.constraints.ui.ConstraintReportPanel( config );
-	}
+	};
 
 	/**
 	 * Build a panel for a single constraint parameter check result.
-	 * This is to `wbcheckconstraintparameters` what {@link buildReport} is to `wbcheckconstraints`.
+	 * This is to `wbcheckconstraintparameters` what {@link this._buildReport} is to `wbcheckconstraints`.
 	 *
 	 * @param {Object} problem The constraint parameter check result.
 	 * @returns {OO.ui.PanelLayout}
 	 */
-	function buildParameterReport( problem ) {
+	SELF.prototype._buildParameterReport = function ( problem ) {
 		var $report, $heading, $body;
 
 		$report = $( '<div>' )
@@ -107,19 +196,19 @@ module.exports = ( function ( mw, wb, $, OO ) {
 			expanded: false,
 			$content: $report
 		} );
-	}
+	};
 
 	/**
 	 * Build a list of constraint reports from a list of panels,
 	 * but return it only if the list is nonempty and contains uncollapsed items.
 	 *
 	 * @param {wikibase.quality.constraints.ui.ConstraintReportPanel[]} reports
-	 * A list of individual report panels as returned by {@link buildReport}.
+	 * A list of individual report panels as returned by {@link this._buildReport}.
 	 * @returns {wikibase.quality.constraints.ui.ConstraintReportList|null}
 	 * The list if it contains at least one uncollapsed panel,
 	 * otherwise null.
 	 */
-	function buildReportList( reports ) {
+	SELF.prototype._buildReportList = function ( reports ) {
 		var list = wikibase.quality.constraints.ui.ConstraintReportList.static.fromPanels(
 			reports,
 			{
@@ -152,7 +241,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 		} else {
 			return null;
 		}
-	}
+	};
 
 	/**
 	 * Extract the constraint check results for a certain statement from the full API response.
@@ -163,7 +252,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 	 * @returns {Object|null} An object containing lists of constraint check results,
 	 * or null if the results could not be extracted.
 	 */
-	function extractResultsForStatement( entityData, propertyId, statementId ) {
+	SELF.prototype._extractResultsForStatement = function ( entityData, propertyId, statementId ) {
 		var statements, index, statement, results = {}, qualifierPID, referenceIndex, referenceHash, referencePID;
 		if ( 'claims' in entityData ) {
 			// API v2 format
@@ -218,7 +307,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 				return null;
 			}
 		}
-	}
+	};
 
 	/**
 	 * Add a popup for the given results to the given snak,
@@ -228,15 +317,15 @@ module.exports = ( function ( mw, wb, $, OO ) {
 	 * @param {jQuery} $snak The snak to which the results apply.
 	 * @returns {boolean} Whether any results were added to the snak or not.
 	 */
-	function addResultsToSnak( results, $snak ) {
-		var reports = results.map( buildReport ),
-			list = buildReportList( reports ),
+	SELF.prototype._addResultsToSnak = function ( results, $snak ) {
+		var reports = results.map( this._buildReport ),
+			list = this._buildReportList( reports ),
 			haveMandatoryViolations;
 
 		if ( list !== null ) {
 			haveMandatoryViolations = list.items[ 0 ].status === 'violation';
 
-			buildPopup(
+			this._buildPopup(
 				list.$element,
 				$snak.find( '.wikibase-snakview-indicators' ),
 				( haveMandatoryViolations ? '' : 'non-' ) + 'mandatory-constraint-violation',
@@ -246,7 +335,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 		} else {
 			return false;
 		}
-	}
+	};
 
 	/**
 	 * Add constraint check results to a certain statement.
@@ -254,13 +343,13 @@ module.exports = ( function ( mw, wb, $, OO ) {
 	 * @param {Object} entityData The constraint check results for a single entity.
 	 * @param {jQuery} $statement The statement.
 	 */
-	function addReportsToStatement( entityData, $statement ) {
+	SELF.prototype._addReportsToStatement = function ( entityData, $statement ) {
 		var match = $statement[ 0 ].className.match(
-				/\bwikibase-statement-([^\s$]+\$[\dA-F-]+)\b/i
+			/\bwikibase-statement-([^\s$]+\$[\dA-F-]+)\b/i
 			),
 			statementId = match && match[ 1 ],
 			propertyId = $statement.parents( '.wikibase-statementgroupview' )[ 0 ].id,
-			results = extractResultsForStatement( entityData, propertyId, statementId ),
+			results = this._extractResultsForStatement( entityData, propertyId, statementId ),
 			index,
 			qualifier,
 			hash,
@@ -271,14 +360,14 @@ module.exports = ( function ( mw, wb, $, OO ) {
 			return;
 		}
 
-		addResultsToSnak(
+		this._addResultsToSnak(
 			results.mainsnak,
 			$statement.find( '.wikibase-statementview-mainsnak' )
 		);
 
 		for ( index in results.qualifiers ) {
 			qualifier = results.qualifiers[ index ];
-			addResultsToSnak(
+			this._addResultsToSnak(
 				qualifier.results,
 				$statement.find(
 					'.wikibase-statementview-qualifiers ' +
@@ -290,7 +379,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 		for ( hash in results.references ) {
 			for ( index in results.references[ hash ] ) {
 				reference = results.references[ hash ][ index ];
-				hasResults = addResultsToSnak(
+				hasResults = this._addResultsToSnak(
 					reference.results,
 					$statement.find(
 						'.wikibase-statementview-references ' +
@@ -309,7 +398,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 				}
 			}
 		}
-	}
+	};
 
 	/**
 	 * Add constraint parameter check results for all constraint statements listed in the reports.
@@ -317,7 +406,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 	 * @param {Object} parameterReports A map from constraint ID (i.e., constraint statement ID)
 	 * to results for that parameter (overall status and list of problems).
 	 */
-	function addParameterReports( parameterReports ) {
+	SELF.prototype._addParameterReports = function ( parameterReports ) {
 		var constraintId,
 			status,
 			problems,
@@ -332,7 +421,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 			}
 
 			problems = parameterReports[ constraintId ].problems;
-			reports = problems.map( buildParameterReport );
+			reports = problems.map( this._buildParameterReport );
 
 			list = new wikibase.quality.constraints.ui.ConstraintReportList( {
 				items: [
@@ -345,8 +434,8 @@ module.exports = ( function ( mw, wb, $, OO ) {
 			} );
 
 			$snak = $( '.wikibase-statement-' + constraintId.replace( /\$/g, '\\$' ) +
-								' .wikibase-statementview-mainsnak .wikibase-snakview' );
-			buildPopup(
+				' .wikibase-statementview-mainsnak .wikibase-snakview' );
+			this._buildPopup(
 				list.$element,
 				$snak.find( '.wikibase-snakview-indicators' ),
 				'alert',
@@ -354,14 +443,14 @@ module.exports = ( function ( mw, wb, $, OO ) {
 				'warning'
 			);
 		}
-	}
+	};
 
 	/**
 	 * Get options for the {@link mediaWiki.Api()} constructor.
 	 *
 	 * @returns {Object} The options.
 	 */
-	function mwApiOptions() {
+	SELF.prototype._mwApiOptions = function () {
 		var gadgetState = mw.loader.getState( 'wikibase.quality.constraints.gadget' );
 		return {
 			ajax: {
@@ -372,94 +461,7 @@ module.exports = ( function ( mw, wb, $, OO ) {
 				}
 			}
 		};
-	}
-
-	function fullCheck( api, lang, entityId ) {
-		mw.track( 'counter.MediaWiki.wikibase.quality.constraints.gadget.loadEntity' );
-		return api.get( {
-			action: 'wbcheckconstraints',
-			format: 'json',
-			formatversion: 2,
-			uselang: lang,
-			id: entityId,
-			status: CACHED_STATUSES
-		} ).then( function ( data ) {
-			$( '.wbqc-reports-button' ).remove();
-			$( '.wikibase-statementgroupview .wikibase-statementview' )
-				.each( function () { addReportsToStatement( data.wbcheckconstraints[ entityId ], $( this ) ); } );
-		} );
-	}
-
-	function snakCheck( api, lang, entityId, statementId ) {
-		var isUpdated = false,
-			statementClass = 'wikibase-statement-' + statementId.replace( /\$/, '\\$$' );
-
-		api.get( {
-			action: 'wbcheckconstraints',
-			format: 'json',
-			formatversion: 2,
-			uselang: lang,
-			claimid: statementId,
-			status: CACHED_STATUSES
-		} ).then( function ( data ) {
-			if ( isUpdated ) {
-				return;
-			}
-
-			$( '.wikibase-statementgroupview .wikibase-statementview.' + statementClass )
-				.each( function () { addReportsToStatement( data.wbcheckconstraints[ entityId ], $( this ) ); } );
-		} );
-		fullCheck( api, lang, entityId ).then( function () {
-			isUpdated = true;
-		} );
-	}
-
-	function propertyParameterCheck( api, lang, entityId ) {
-		mw.track( 'counter.MediaWiki.wikibase.quality.constraints.gadget.loadProperty' );
-		return api.get( {
-			action: 'wbcheckconstraintparameters',
-			format: 'json',
-			formatversion: 2,
-			uselang: lang,
-			propertyid: entityId
-		} ).then( function ( data ) {
-			addParameterReports( data.wbcheckconstraintparameters[ entityId ] );
-		} );
-	}
-
-	function SELF() {
-		this.defaultBehavior = function () {
-			var entityId = mw.config.get( 'wbEntityId' );
-
-			if ( entityId === null || mw.config.get( 'wgMFMode' ) || !mw.config.get( 'wbIsEditView' ) ) {
-				// no entity, mobile frontend, or not editing (diff, oldid, …) – skip
-				return;
-			}
-
-			mw.loader.using( [
-				'mediawiki.api',
-				'oojs-ui-core',
-				'oojs-ui-widgets',
-				'oojs-ui.styles.icons-alerts',
-				'wikibase.quality.constraints.icon',
-				'wikibase.quality.constraints.ui'
-			] ).done( function () {
-				var api = new mw.Api( mwApiOptions() ),
-					lang = mw.config.get( 'wgUserLanguage' );
-
-				fullCheck( api, lang, entityId );
-
-				if ( mw.config.get( 'wgPageContentModel' ) === 'wikibase-property' ) {
-					propertyParameterCheck( api, lang, entityId );
-				}
-
-				mw.hook( 'wikibase.statement.saved' ).add( function ( entityId, statementId ) {
-					mw.track( 'counter.MediaWiki.wikibase.quality.constraints.gadget.saveStatement' );
-					snakCheck( api, lang, entityId, statementId );
-				} );
-			} );
-		};
-	}
+	};
 
 	if ( typeof exports === 'undefined' ) {
 		( new SELF() ).defaultBehavior();
