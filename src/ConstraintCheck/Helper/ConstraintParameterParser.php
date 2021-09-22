bc3a978577efc7057916739cd7e8f4b8cedac131
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2021-09-22 07:31:53 +0100

    FP: tighten (De)SerializerFactory typings again
    Now using the classes moved to the correct namespace
    
    Bug: T289040
    Depends-On: I11bc3856c865ed53cff75592f6d6036bdbea9ec5
    Change-Id: I1c8015342d52ca55ca222ab60035b6dd9a52d039

--- src/ConstraintCheck/Helper/ConstraintParameterParser.php
+++ src/ConstraintCheck/Helper/ConstraintParameterParser.php
@@ -9,6 +9,7 @@ use DataValues\MultilingualTextValue;
 use DataValues\StringValue;
 use DataValues\UnboundedQuantityValue;
 use LogicException;
+use Wikibase\DataModel\Deserializers\DeserializerFactory;
 use Wikibase\DataModel\Deserializers\SnakDeserializer;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\EntityIdValue;
@@ -57,15 +58,14 @@ class ConstraintParameterParser {
 	/**
 	 * @param Config $config
 	 *   contains entity IDs used in constraint parameters (constraint statement qualifiers)
-	 * @param $factory
+	 * @param DeserializerFactory $factory
 	 *   used to parse constraint statement qualifiers into constraint parameters
 	 * @param string $unitItemConceptBaseUri
 	 *   concept base URI of items used for units
 	 */
-	// phpcs:ignore MediaWiki.Commenting.FunctionComment.NoParamType -- For T289040
 	public function __construct(
 		Config $config,
-		$factory,
+		DeserializerFactory $factory,
 		string $unitItemConceptBaseUri
 	) {
 		$this->config = $config;
