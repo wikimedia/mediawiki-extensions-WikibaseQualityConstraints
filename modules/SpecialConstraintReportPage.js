aa4c74888691c7f49c8b7422ef1367697754b5c7
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-07-25 18:10:11 +0200

    Remove expandable content scripts+styles
    No longer used.
    
    Bug: T338593
    Change-Id: Iaa2ae0e3e7a3d85f97a3e6c347f694b710804297

--- modules/SpecialConstraintReportPage.js
+++
@@ -1,9 +0,0 @@
-( function ( $ ) {
-	'use strict';
-
-	$( function () {
-		$( '.wbqc-expandable-content-indicator' ).on( 'click', function () {
-			$( this ).closest( 'td' ).find( '.wbqc-expandable-content' ).slideToggle( 'fast' );
-		} );
-	} );
-}( jQuery ) );
