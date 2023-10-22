1537bf8b501b583d2b9c7b61bf645c57065a68e6
Umherirrender <umherirrender_de.wp@web.de>
2023-10-22 11:34:20 +0200

    Replace empty() with falsy check
    empty() should only be used to suppress errors
    When the type of the variable is array,
    a falsy check is the same (checks for null, false and empty array)
    Found by a new phan plugin (T234237)
    
    Change-Id: I23c63d221dad3c3bc542d84da7535c4645693826

--- src/ConstraintCheck/Helper/ConstraintParameterParser.php
+++ src/ConstraintCheck/Helper/ConstraintParameterParser.php
@@ -923,7 +923,7 @@ class ConstraintParameterParser {
 			$entityTypeItemIds[] = $itemId;
 		}
 
-		if ( empty( $entityTypes ) ) {
+		if ( !$entityTypes ) {
 			// @codeCoverageIgnoreStart
 			throw new LogicException(
 				'The "entity types" parameter is required, ' .
@@ -994,7 +994,7 @@ class ConstraintParameterParser {
 			$contextTypes[] = $this->mapItemId( $itemId, $mapping, $parameterId );
 		}
 
-		if ( empty( $contextTypes ) ) {
+		if ( !$contextTypes ) {
 			// @codeCoverageIgnoreStart
 			throw new LogicException(
 				'The "property scope" parameter is required, ' .
