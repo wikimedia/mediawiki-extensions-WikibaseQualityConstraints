4e89cae63b0e511b424082b6babf9fb7222722c9
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2026-08-24 17:37:07 +0200

    Extract ConstraintReportTableBuilder class
    This just moves the code (with as few changes as possible), without much
    improving it yet; I’ll do that in a separate change, to keep it easier
    to review. (To confirm that the code mostly just moved without changes,
    I recommend the `--color-moved=dimmed-zebra` flag to `git show`.)
    
    The msg() method is just a shortcut because we call it so much and I
    didn’t want to change each call to $this->messageLocalizer->msg(); I
    decided to make the class implement MessageLocalizer itself because this
    way I can just use /** @inheritDoc */ to silence PHPCS warnings about
    the documentation.
    
    This effectively supersedes change Ie5fa5a4c5a (where I both moved and
    improved the code in one commit, making it harder to review or rebase).
    
    Change-Id: I8e5ccba547d0b8d4fcbaddaabf6b524c5fb1f6f6

---
+++ src/Specials/ConstraintReportTableBuilder.php
@@ -0,0 +1,258 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WikibaseQuality\ConstraintReport\Specials;
+
+use InvalidArgumentException;
+use MediaWiki\Config\Config;
+use MediaWiki\Html\Html;
+use MediaWiki\Language\Language;
+use MediaWiki\Language\MessageLocalizer;
+use MediaWiki\Message\Message;
+use OOUI\IconWidget;
+use OOUI\LabelWidget;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Entity\NumericPropertyId;
+use Wikibase\DataModel\Services\EntityId\EntityIdFormatter;
+use Wikibase\Lib\Store\EntityTitleLookup;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Message\ViolationMessageRenderer;
+use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\CheckResult;
+use WikibaseQuality\ConstraintReport\Html\HtmlTableBuilder;
+use WikibaseQuality\ConstraintReport\Html\HtmlTableCellBuilder;
+use WikibaseQuality\ConstraintReport\Html\HtmlTableHeaderBuilder;
+use Wikimedia\HtmlArmor\HtmlArmor;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class ConstraintReportTableBuilder implements MessageLocalizer {
+
+	public function __construct(
+		private Language $language,
+		private MessageLocalizer $messageLocalizer,
+		private EntityTitleLookup $entityTitleLookup,
+		private EntityIdFormatter $entityIdLabelFormatter,
+		private EntityIdFormatter $entityIdLinkFormatter,
+		private ViolationMessageRenderer $violationMessageRenderer,
+		private Config $config,
+	) {
+	}
+
+	/** @inheritDoc */
+	public function msg( $key, ...$params ): Message {
+		return $this->messageLocalizer->msg( $key, ...$params );
+	}
+
+	/**
+	 * Returns html text of the result header
+	 *
+	 * @param EntityId $entityId
+	 *
+	 * @return string HTML
+	 */
+	public function buildResultHeader( EntityId $entityId ): string {
+		return Html::rawElement(
+			'h3',
+			[],
+			$this->msg( 'wbqc-constraintreport-result-headline' )->escaped() .
+				$this->msg( 'word-separator' )->escaped() .
+				$this->entityIdLinkFormatter->formatEntityId( $entityId ) .
+				$this->msg( 'word-separator' )->escaped() .
+				$this->msg( 'parentheses', $entityId->getSerialization() )->escaped()
+		);
+	}
+
+	/**
+	 * Builds summary from given results
+	 *
+	 * @param CheckResult[] $results
+	 *
+	 * @return string HTML
+	 */
+	public function buildSummary( array $results ): string {
+		$statuses = [];
+		foreach ( $results as $result ) {
+			$status = strtolower( $result->getStatus() );
+			$statuses[$status] ??= 0;
+			$statuses[$status]++;
+		}
+
+		$statusElements = [];
+		foreach ( $statuses as $status => $count ) {
+			$statusElements[] = $this->formatStatus( $status ) .
+				$this->msg( 'colon-separator' )->escaped() .
+				htmlspecialchars( $this->language->formatNum( $count ) );
+		}
+
+		return Html::rawElement( 'p', [],
+			implode( $this->msg( 'comma-separator' )->escaped(), $statusElements )
+		);
+	}
+
+	/**
+	 * @param EntityId $entityId
+	 * @param CheckResult[] $results
+	 *
+	 * @return string HTML
+	 */
+	public function buildResultTable( EntityId $entityId, array $results ): string {
+		// Set table headers
+		$table = new HtmlTableBuilder(
+			[
+				new HtmlTableHeaderBuilder(
+					$this->msg( 'wbqc-constraintreport-result-table-header-status' )->text(),
+					true
+				),
+				new HtmlTableHeaderBuilder(
+					$this->msg( 'wbqc-constraintreport-result-table-header-property' )->text(),
+					true
+				),
+				new HtmlTableHeaderBuilder(
+					$this->msg( 'wbqc-constraintreport-result-table-header-message' )->text(),
+					true
+				),
+				new HtmlTableHeaderBuilder(
+					$this->msg( 'wbqc-constraintreport-result-table-header-constraint' )->text(),
+					true
+				),
+			]
+		);
+
+		foreach ( $results as $result ) {
+			$this->appendToResultTable( $table, $entityId, $result );
+		}
+
+		return $table->toHtml();
+	}
+
+	private function appendToResultTable(
+		HtmlTableBuilder $table,
+		EntityId $entityId,
+		CheckResult $result
+	): void {
+		$message = $result->getMessage();
+		if ( !$message ) {
+			// no row for this result
+			return;
+		}
+
+		// Status column
+		$statusColumn = $this->formatStatus( $result->getStatus() );
+
+		// Property column
+		$propertyId = new NumericPropertyId( $result->getContextCursor()->getSnakPropertyId() );
+		$propertyColumn = $this->getClaimLink(
+			$entityId,
+			$propertyId,
+			$this->entityIdLabelFormatter->formatEntityId( $propertyId )
+		);
+
+		// Message column
+		$messageColumn = $this->violationMessageRenderer->render( $message );
+
+		// Constraint column
+		$constraintTypeItemId = $result->getConstraint()->getConstraintTypeItemId();
+		try {
+			$constraintTypeLabel = $this->entityIdLabelFormatter->formatEntityId( new ItemId( $constraintTypeItemId ) );
+		} catch ( InvalidArgumentException ) {
+			$constraintTypeLabel = htmlspecialchars( $constraintTypeItemId );
+		}
+		$constraintColumn = $this->getClaimLink(
+			$propertyId,
+			new NumericPropertyId( $this->config->get( 'WBQualityConstraintsPropertyConstraintId' ) ),
+			$constraintTypeLabel
+		);
+
+		// Append cells
+		$table->appendRow(
+			[
+				new HtmlTableCellBuilder(
+					new HtmlArmor( $statusColumn )
+				),
+				new HtmlTableCellBuilder(
+					new HtmlArmor( $propertyColumn )
+				),
+				new HtmlTableCellBuilder(
+					new HtmlArmor( $messageColumn )
+				),
+				new HtmlTableCellBuilder(
+					new HtmlArmor( $constraintColumn )
+				),
+			]
+		);
+	}
+
+	/**
+	 * Formats given status to html
+	 *
+	 * @param string $status
+	 *
+	 * @throws InvalidArgumentException
+	 *
+	 * @return string HTML
+	 */
+	private function formatStatus( string $status ): string {
+		$messageName = "wbqc-constraintreport-status-" . strtolower( $status );
+		$statusIcons = [
+			CheckResult::STATUS_SUGGESTION => [
+				'icon' => 'suggestion-constraint-violation',
+			],
+			CheckResult::STATUS_WARNING => [
+				'icon' => 'non-mandatory-constraint-violation',
+			],
+			CheckResult::STATUS_VIOLATION => [
+				'icon' => 'mandatory-constraint-violation',
+			],
+			CheckResult::STATUS_BAD_PARAMETERS => [
+				'icon' => 'alert',
+				'flags' => 'warning',
+			],
+		];
+
+		if ( array_key_exists( $status, $statusIcons ) ) {
+			$iconHtml = new IconWidget( $statusIcons[$status] ) .
+				$this->msg( 'word-separator' )->escaped();
+		} else {
+			$iconHtml = '';
+		}
+
+		$labelWidget = new LabelWidget( [ 'label' => $this->msg( $messageName )->text() ] );
+
+		return Html::rawElement(
+			'span',
+			[
+				'class' => 'wbqc-status wbqc-status-' . $status,
+			],
+			$iconHtml . $labelWidget
+		);
+	}
+
+	/**
+	 * Returns html link to given entity with anchor to specified property.
+	 *
+	 * @param EntityId $entityId
+	 * @param NumericPropertyId $propertyId
+	 * @param string $text HTML
+	 *
+	 * @return string HTML
+	 */
+	private function getClaimLink(
+		EntityId $entityId,
+		NumericPropertyId $propertyId,
+		string $text
+	): string {
+		$title = clone $this->entityTitleLookup->getTitleForId( $entityId );
+		$title->setFragment( $propertyId->getSerialization() );
+
+		return Html::rawElement( 'a',
+			[
+				'href' => $title->getLinkURL(),
+				'target' => '_blank',
+			],
+			$text
+		);
+	}
+
+}
--- src/Specials/SpecialConstraintReport.php
+++ src/Specials/SpecialConstraintReport.php
@@ -10,14 +10,9 @@ use MediaWiki\Html\Html;
 use MediaWiki\HTMLForm\Field\HTMLTextField;
 use MediaWiki\HTMLForm\HTMLForm;
 use MediaWiki\SpecialPage\SpecialPage;
-use OOUI\IconWidget;
-use OOUI\LabelWidget;
 use UnexpectedValueException;
-use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\EntityIdParser;
 use Wikibase\DataModel\Entity\EntityIdParsingException;
-use Wikibase\DataModel\Entity\ItemId;
-use Wikibase\DataModel\Entity\NumericPropertyId;
 use Wikibase\DataModel\Services\EntityId\EntityIdFormatter;
 use Wikibase\DataModel\Services\Lookup\EntityLookup;
 use Wikibase\Lib\LanguageFallbackChainFactory;
@@ -27,11 +22,6 @@ use Wikibase\View\EntityIdFormatterFactory;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\DelegatingConstraintChecker;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Message\ViolationMessageRenderer;
 use WikibaseQuality\ConstraintReport\ConstraintCheck\Message\ViolationMessageRendererFactory;
-use WikibaseQuality\ConstraintReport\ConstraintCheck\Result\CheckResult;
-use WikibaseQuality\ConstraintReport\Html\HtmlTableBuilder;
-use WikibaseQuality\ConstraintReport\Html\HtmlTableCellBuilder;
-use WikibaseQuality\ConstraintReport\Html\HtmlTableHeaderBuilder;
-use Wikimedia\HtmlArmor\HtmlArmor;
 use Wikimedia\Stats\StatsFactory;
 
 /**
@@ -218,17 +208,18 @@ class SpecialConstraintReport extends SpecialPage {
 			->increment();
 		$results = $this->constraintChecker->checkAgainstConstraintsOnEntityId( $entityId );
 
+		$constraintReportTableBuilder = $this->getConstraintReportTableBuilder();
 		if ( !$results ) {
-			$out->addHTML( $this->buildResultHeader( $entityId ) .
+			$out->addHTML( $constraintReportTableBuilder->buildResultHeader( $entityId ) .
 				$this->buildNotice( 'wbqc-constraintreport-empty-result' )
 			);
 			return;
 		}
 
 		$out->addHTML(
-			$this->buildResultHeader( $entityId )
-			. $this->buildSummary( $results )
-			. $this->buildResultTable( $entityId, $results )
+			$constraintReportTableBuilder->buildResultHeader( $entityId )
+			. $constraintReportTableBuilder->buildSummary( $results )
+			. $constraintReportTableBuilder->buildResultTable( $entityId, $results )
 		);
 	}
 
@@ -298,213 +289,15 @@ class SpecialConstraintReport extends SpecialPage {
 		);
 	}
 
-	/**
-	 * @param EntityId $entityId
-	 * @param CheckResult[] $results
-	 *
-	 * @return string HTML
-	 */
-	private function buildResultTable( EntityId $entityId, array $results ): string {
-		// Set table headers
-		$table = new HtmlTableBuilder(
-			[
-				new HtmlTableHeaderBuilder(
-					$this->msg( 'wbqc-constraintreport-result-table-header-status' )->text(),
-					true
-				),
-				new HtmlTableHeaderBuilder(
-					$this->msg( 'wbqc-constraintreport-result-table-header-property' )->text(),
-					true
-				),
-				new HtmlTableHeaderBuilder(
-					$this->msg( 'wbqc-constraintreport-result-table-header-message' )->text(),
-					true
-				),
-				new HtmlTableHeaderBuilder(
-					$this->msg( 'wbqc-constraintreport-result-table-header-constraint' )->text(),
-					true
-				),
-			]
-		);
-
-		foreach ( $results as $result ) {
-			$this->appendToResultTable( $table, $entityId, $result );
-		}
-
-		return $table->toHtml();
-	}
-
-	private function appendToResultTable(
-		HtmlTableBuilder $table,
-		EntityId $entityId,
-		CheckResult $result
-	): void {
-		$message = $result->getMessage();
-		if ( !$message ) {
-			// no row for this result
-			return;
-		}
-
-		// Status column
-		$statusColumn = $this->formatStatus( $result->getStatus() );
-
-		// Property column
-		$propertyId = new NumericPropertyId( $result->getContextCursor()->getSnakPropertyId() );
-		$propertyColumn = $this->getClaimLink(
-			$entityId,
-			$propertyId,
-			$this->entityIdLabelFormatter->formatEntityId( $propertyId )
-		);
-
-		// Message column
-		$messageColumn = $this->violationMessageRenderer->render( $message );
-
-		// Constraint column
-		$constraintTypeItemId = $result->getConstraint()->getConstraintTypeItemId();
-		try {
-			$constraintTypeLabel = $this->entityIdLabelFormatter->formatEntityId( new ItemId( $constraintTypeItemId ) );
-		} catch ( InvalidArgumentException ) {
-			$constraintTypeLabel = htmlspecialchars( $constraintTypeItemId );
-		}
-		$constraintColumn = $this->getClaimLink(
-			$propertyId,
-			new NumericPropertyId( $this->config->get( 'WBQualityConstraintsPropertyConstraintId' ) ),
-			$constraintTypeLabel
-		);
-
-		// Append cells
-		$table->appendRow(
-			[
-				new HtmlTableCellBuilder(
-					new HtmlArmor( $statusColumn )
-				),
-				new HtmlTableCellBuilder(
-					new HtmlArmor( $propertyColumn )
-				),
-				new HtmlTableCellBuilder(
-					new HtmlArmor( $messageColumn )
-				),
-				new HtmlTableCellBuilder(
-					new HtmlArmor( $constraintColumn )
-				),
-			]
-		);
-	}
-
-	/**
-	 * Returns html text of the result header
-	 *
-	 * @param EntityId $entityId
-	 *
-	 * @return string HTML
-	 */
-	protected function buildResultHeader( EntityId $entityId ): string {
-		return Html::rawElement(
-			'h3',
-			[],
-			$this->msg( 'wbqc-constraintreport-result-headline' )->escaped() .
-				$this->msg( 'word-separator' )->escaped() .
-				$this->entityIdLinkFormatter->formatEntityId( $entityId ) .
-				$this->msg( 'word-separator' )->escaped() .
-				$this->msg( 'parentheses', $entityId->getSerialization() )->escaped()
-		);
-	}
-
-	/**
-	 * Builds summary from given results
-	 *
-	 * @param CheckResult[] $results
-	 *
-	 * @return string HTML
-	 */
-	protected function buildSummary( array $results ): string {
-		$statuses = [];
-		foreach ( $results as $result ) {
-			$status = strtolower( $result->getStatus() );
-			$statuses[$status] ??= 0;
-			$statuses[$status]++;
-		}
-
-		$statusElements = [];
-		foreach ( $statuses as $status => $count ) {
-			$statusElements[] = $this->formatStatus( $status ) .
-				$this->msg( 'colon-separator' )->escaped() .
-				htmlspecialchars( $this->getLanguage()->formatNum( $count ) );
-		}
-
-		return Html::rawElement( 'p', [],
-			implode( $this->msg( 'comma-separator' )->escaped(), $statusElements )
-		);
-	}
-
-	/**
-	 * Formats given status to html
-	 *
-	 * @param string $status
-	 *
-	 * @throws InvalidArgumentException
-	 *
-	 * @return string HTML
-	 */
-	private function formatStatus( string $status ): string {
-		$messageName = "wbqc-constraintreport-status-" . strtolower( $status );
-		$statusIcons = [
-			CheckResult::STATUS_SUGGESTION => [
-				'icon' => 'suggestion-constraint-violation',
-			],
-			CheckResult::STATUS_WARNING => [
-				'icon' => 'non-mandatory-constraint-violation',
-			],
-			CheckResult::STATUS_VIOLATION => [
-				'icon' => 'mandatory-constraint-violation',
-			],
-			CheckResult::STATUS_BAD_PARAMETERS => [
-				'icon' => 'alert',
-				'flags' => 'warning',
-			],
-		];
-
-		if ( array_key_exists( $status, $statusIcons ) ) {
-			$iconHtml = new IconWidget( $statusIcons[$status] ) .
-				$this->msg( 'word-separator' )->escaped();
-		} else {
-			$iconHtml = '';
-		}
-
-		$labelWidget = new LabelWidget( [ 'label' => $this->msg( $messageName )->text() ] );
-
-		return Html::rawElement(
-			'span',
-			[
-				'class' => 'wbqc-status wbqc-status-' . $status,
-			],
-			$iconHtml . $labelWidget
-		);
-	}
-
-	/**
-	 * Returns html link to given entity with anchor to specified property.
-	 *
-	 * @param EntityId $entityId
-	 * @param NumericPropertyId $propertyId
-	 * @param string $text HTML
-	 *
-	 * @return string HTML
-	 */
-	private function getClaimLink(
-		EntityId $entityId,
-		NumericPropertyId $propertyId,
-		string $text
-	): string {
-		$title = clone $this->entityTitleLookup->getTitleForId( $entityId );
-		$title->setFragment( $propertyId->getSerialization() );
-
-		return Html::rawElement( 'a',
-			[
-				'href' => $title->getLinkURL(),
-				'target' => '_blank',
-			],
-			$text
+	private function getConstraintReportTableBuilder(): ConstraintReportTableBuilder {
+		return new ConstraintReportTableBuilder(
+			$this->getLanguage(),
+			$this,
+			$this->entityTitleLookup,
+			$this->entityIdLabelFormatter,
+			$this->entityIdLinkFormatter,
+			$this->violationMessageRenderer,
+			$this->config,
 		);
 	}
 
